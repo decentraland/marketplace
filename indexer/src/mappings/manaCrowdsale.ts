40e09e227099a7c4b688d7e4b93528dae27a6d2f
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-12-19 14:11:42 -0300

    feat: indexer

---
+++ indexer/src/mappings/manaCrowdsale.ts
@@ -0,0 +1,17 @@
+import { Address } from '@graphprotocol/graph-ts'
+
+import { Finalized } from '../types/MANACrowdsale/MANACrowdsale'
+import { ERC721 } from '../types/templates'
+import {
+  LANDRegistry,
+  EstateRegistry,
+  ERC721Collection_halloween_2019,
+  ERC721Collection_exclusive_masks
+} from '../utils/addresses'
+
+export function handleFinalized(_: Finalized): void {
+  ERC721.create(Address.fromString(LANDRegistry))
+  ERC721.create(Address.fromString(EstateRegistry))
+  ERC721.create(Address.fromString(ERC721Collection_halloween_2019))
+  ERC721.create(Address.fromString(ERC721Collection_exclusive_masks))
+}
---
+++ indexer/src/utils/categories.ts
@@ -0,0 +1,3 @@
+export const PARCEL = 'parcel'
+export const ESTATE = 'estate'
+export const WEARABLE = 'wearable'
