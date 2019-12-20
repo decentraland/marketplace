05026156bc9d1278f0d289d34bcb245f7b477249
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-12-20 17:08:57 -0300

    feat: rename folders and ignore built files

--- indexer/src/handlers/nft.ts
+++ indexer/src/handlers/nft.ts
@@ -1,14 +1,9 @@
 import { Transfer } from '../entities/templates/ERC721/ERC721'
-import { NFT, Parcel, Estate } from '../entities/schema'
+import { NFT } from '../entities/schema'
 import { isMint, buildId, getTokenURI } from '../modules/nft'
+import { getUpdatedMetricEntity } from '../modules/metric'
 import { getCategory } from '../modules/category'
-import { buildData, DataType } from '../modules/data'
-import { upsertMetric } from '../modules/metric'
-import { decodeTokenId } from '../modules/parcel'
-import { upsertWearable } from '../modules/wearable'
-import { createWallet } from '../modules/wallet'
 import * as addresses from '../modules/contract/addresses'
-import * as categories from '../modules/category/categories'
 
 export function handleTransfer(event: Transfer): void {
   if (event.params.tokenId.toString() == '') {
@@ -22,30 +17,18 @@ export function handleTransfer(event: Transfer): void {
   let nft = new NFT(id)
 
   nft.tokenId = event.params.tokenId
-  nft.owner = event.params.to.toHex()
+  nft.owner = event.params.to
   nft.contractAddress = event.address
   nft.category = category
-
   if (contractAddress != addresses.LANDRegistry) {
     // The LANDRegistry contract does not have a tokenURI method
     nft.tokenURI = getTokenURI(event)
   }
 
-  if (category == categories.WEARABLE) {
-    let wearableId = upsertWearable(nft.tokenURI)
-    if (wearableId != '') {
-      nft.wearable = wearableId
-    }
-  }
+  nft.save()
 
   if (isMint(event)) {
-    upsertMetric(contractAddress)
-    nft.createdAt = event.block.timestamp
-  } else {
-    nft.updatedAt = event.block.timestamp
+    let metric = getUpdatedMetricEntity(contractAddress)
+    metric.save()
   }
-
-  createWallet(event.params.to.toHex())
-
-  nft.save()
 }
