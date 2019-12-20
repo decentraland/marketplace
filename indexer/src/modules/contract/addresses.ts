1fcefc48773b98158bccfa610c4b5d86b541aec1
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-12-20 16:59:03 -0300

    feat: index xmas

--- indexer/src/modules/contract/addresses.ts
+++ indexer/src/modules/contract/addresses.ts
@@ -7,5 +7,7 @@ export const ERC721Collection_halloween_2019 =
   '0xc1f4b0eea2bd6690930e6c66efd3e197d620b9c2'
 export const ERC721Collection_exclusive_masks =
   '0xc04528c14c8ffd84c7c1fb6719b4a89853035cdd'
+export const ERC721Collection_xmas_2019 =
+  '0x34a4a5631a263823025e55a5c6ad068732e07a37'
 
 export const Marketplace = '0x8e5660b4Ab70168b5a6fEeA0e0315cb49c8Cd539'
