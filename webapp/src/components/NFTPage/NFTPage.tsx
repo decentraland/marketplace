c0c0a021100e42a3f32711d756f3e688b20eec9d
Juan Cazala <juan@decentraland.org>
2020-01-22 17:49:57 -0300

    chore: inline NFTDetail component

--- webapp/src/components/NFTPage/NFTPage.tsx
+++ webapp/src/components/NFTPage/NFTPage.tsx
@@ -2,34 +2,26 @@ import React from 'react'
 import { Page } from 'decentraland-ui'
 import { Navbar, Footer } from 'decentraland-dapps/dist/containers'
 
-import { NFT } from '../../modules/nft/types'
-
 import { NFTProvider } from '../NFTProvider'
 import { ParcelDetail } from './ParcelDetail'
 import { EstateDetail } from './EstateDetail'
 import { WearableDetail } from './WearableDetail'
 import './NFTPage.css'
 
-const NFTDetail = (props: { nft: NFT }) => {
-  const { nft } = props
-  if (nft.parcel) {
-    return <ParcelDetail nft={nft} />
-  }
-  if (nft.estate) {
-    return <EstateDetail nft={nft} />
-  }
-  if (nft.wearable) {
-    return <WearableDetail nft={nft} />
-  }
-  return null
-}
-
 const NFTPage = () => {
   return (
     <>
       <Navbar isFullscreen activePage="marketplace" />
       <Page className="NFTPage" isFullscreen>
-        <NFTProvider>{nft => <NFTDetail nft={nft} />}</NFTProvider>
+        <NFTProvider>
+          {nft => (
+            <>
+              {nft.parcel ? <ParcelDetail nft={nft} /> : null}
+              {nft.estate ? <EstateDetail nft={nft} /> : null}
+              {nft.wearable ? <WearableDetail nft={nft} /> : null}
+            </>
+          )}
+        </NFTProvider>
       </Page>
       <Footer />
     </>
