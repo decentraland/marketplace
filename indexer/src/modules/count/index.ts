457a3813ebc68d602360bba3da7a99a3c8d78ff0
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-01-23 19:08:53 -0300

    chore: camelcase Counts

--- indexer/src/modules/count/index.ts
+++ indexer/src/modules/count/index.ts
@@ -9,19 +9,19 @@ export function buildCount(): Count {
 
   if (count == null) {
     count = new Count(DEFAULT_ID)
-    count.order_total = 0
-    count.order_parcel = 0
-    count.order_estate = 0
-    count.order_wearable = 0
+    count.orderTotal = 0
+    count.orderParcel = 0
+    count.orderEstate = 0
+    count.orderWearable = 0
 
-    count.parcel_total = 0
+    count.parcelTotal = 0
 
-    count.estate_total = 0
+    count.estateTotal = 0
 
-    count.wearable_total = 0
-    count.wearable_halloween_2019 = 0
-    count.wearable_exclusive_masks = 0
-    count.wearable_xmas_2019 = 0
+    count.wearableTotal = 0
+    count.wearableHalloween2019 = 0
+    count.wearableExclusiveMasks = 0
+    count.wearableXmas2019 = 0
   }
 
   return count as Count
@@ -33,18 +33,18 @@ export function buildCountFromNFT(nft: NFT): Count {
   let count = buildCount()
 
   if (category == categories.PARCEL) {
-    count.parcel_total += 1
+    count.parcelTotal += 1
   } else if (category == categories.ESTATE) {
-    count.estate_total += 1
+    count.estateTotal += 1
   } else if (category == categories.WEARABLE) {
-    count.wearable_total += 1
+    count.wearableTotal += 1
 
     if (contractAddress == addresses.Halloween2019Collection) {
-      count.wearable_halloween_2019 += 1
+      count.wearableHalloween2019 += 1
     } else if (contractAddress == addresses.ExclusiveMasksCollection) {
-      count.wearable_exclusive_masks += 1
+      count.wearableExclusiveMasks += 1
     } else if (contractAddress == addresses.Xmas2019Collection) {
-      count.wearable_xmas_2019 += 1
+      count.wearableXmas2019 += 1
     }
   }
 
@@ -54,14 +54,14 @@ export function buildCountFromNFT(nft: NFT): Count {
 export function buildCountFromOrder(order: Order): Count {
   let category = order.category
   let count = buildCount()
-  count.order_total += 1
+  count.orderTotal += 1
 
   if (category == categories.PARCEL) {
-    count.order_parcel += 1
+    count.orderParcel += 1
   } else if (category == categories.ESTATE) {
-    count.order_estate += 1
+    count.orderEstate += 1
   } else if (category == categories.WEARABLE) {
-    count.order_wearable += 1
+    count.orderWearable += 1
   }
   return count
 }
