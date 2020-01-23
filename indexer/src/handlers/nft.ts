5fd7ac4774cc95ea822faf2aa34af3594ec1b985
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-01-23 12:23:07 -0300

    WIP-LASH

--- indexer/src/handlers/nft.ts
+++ indexer/src/handlers/nft.ts
@@ -11,6 +11,21 @@ import { createWallet } from '../modules/wallet'
 import * as categories from '../modules/category/categories'
 import * as addresses from '../data/addresses'
 
+import { NameRegistered } from '../entities/DCLRegistrar/DCLRegistrar'
+import { NameRegistration } from '../entities/schema'
+
+export function handleNameRegistered(event: NameRegistered): void {
+  let nameRegistered = new NameRegistration(
+    event.params._labelHash.toHexString()
+  )
+  nameRegistered.caller = event.params._caller
+  nameRegistered.beneficiary = event.params._beneficiary
+  nameRegistered.labelHash = event.params._labelHash
+  nameRegistered.subdomain = event.params._subdomain
+  nameRegistered.createdAt = event.params._createdDate
+  nameRegistered.save()
+}
+
 export function handleTransfer(event: Transfer): void {
   if (event.params.tokenId.toString() == '') {
     return
