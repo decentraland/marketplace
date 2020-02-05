d005ecb7bb7a10224e565de0a0e457c80ba92471
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-02-05 15:12:03 -0300

    feat: address component

--- webapp/src/components/TransactionHistory/TransactionHistory.tsx
+++ webapp/src/components/TransactionHistory/TransactionHistory.tsx
@@ -1,18 +1,13 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Table, Mana } from 'decentraland-ui'
+import { Table, Loader, Mana } from 'decentraland-ui'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import dateFnsFormat from 'date-fns/format'
 
-import { locations } from '../../modules/routing/locations'
 import { Order } from '../../modules/order/types'
 import { formatMANA } from '../../lib/mana'
 import { nftAPI } from '../../lib/api/nft'
-import { shortenAddress } from '../../modules/wallet/utils'
-import { Owner } from '../NFTPage/Owner'
-
+import { Address } from '../Address'
 import { Props } from './TransactionHistory.types'
-
 import './TransactionHistory.css'
 
 const INPUT_FORMAT = 'PPP'
@@ -34,55 +29,43 @@ const TransactionHistory = (props: Props) => {
     }
   }, [setIsLoading, setOrders])
 
-  if (isLoading) {
-    return null
-  }
-
   return (
-    <Table basic="very">
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>{t('detail.history.from')}</Table.HeaderCell>
-          <Table.HeaderCell>{t('detail.history.to')}</Table.HeaderCell>
-          <Table.HeaderCell>{t('detail.history.when')}</Table.HeaderCell>
-          <Table.HeaderCell>{t('detail.price')}</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
+    <div className="TransactionHistory">
+      {isLoading ? (
+        <Loader active size="massive" />
+      ) : orders.length > 0 ? (
+        <Table basic="very">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>{t('detail.history.from')}</Table.HeaderCell>
+              <Table.HeaderCell>{t('detail.history.to')}</Table.HeaderCell>
+              <Table.HeaderCell>{t('detail.history.when')}</Table.HeaderCell>
+              <Table.HeaderCell>{t('detail.price')}</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-      <Table.Body>
-        {orders.map((order, index) => (
-          <Table.Row key={index}>
-            <Table.Cell>
-              <TransactionAddress nft={nft} address={order.owner} />
-            </Table.Cell>
-            <Table.Cell>
-              <TransactionAddress nft={nft} address={order.buyer} />
-            </Table.Cell>
-            <Table.Cell>
-              {dateFnsFormat(+order.createdAt * 1000, INPUT_FORMAT)}
-            </Table.Cell>
-            <Table.Cell>
-              <Mana inline>{formatMANA(order.price)}</Mana>
-            </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+          <Table.Body>
+            {orders.map((order, index) => (
+              <Table.Row key={index}>
+                <Table.Cell>
+                  <Address address={order.owner} />
+                </Table.Cell>
+                <Table.Cell>
+                  <Address address={order.buyer} />
+                </Table.Cell>
+                <Table.Cell>
+                  {dateFnsFormat(+order.createdAt * 1000, INPUT_FORMAT)}
+                </Table.Cell>
+                <Table.Cell>
+                  <Mana inline>{formatMANA(order.price)}</Mana>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      ) : null}
+    </div>
   )
 }
 
 export default React.memo(TransactionHistory)
-
-const TransactionAddress = (props: any) => {
-  const { address, nft } = props
-
-  return (
-    <div className="address-wrapper" title={address}>
-      <Owner nft={nft} />
-      <Link to={locations.account(address)}>
-        &nbsp;
-        <span className="short-address">{shortenAddress(address)}</span>
-      </Link>
-    </div>
-  )
-}
