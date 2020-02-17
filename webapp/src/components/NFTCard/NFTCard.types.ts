db584c4bbabc87b9e10eaad7ab54d32867bc0f90
Nicolas Santangelo <santangelonicolas@gmail.com>
2020-02-16 23:35:17 -0300

    feat: add actual links to allow for "open in new tab" (#117)
    Co-authored-by: Juan Cazala <juancazala@gmail.com>

--- webapp/src/components/NFTCard/NFTCard.types.ts
+++ webapp/src/components/NFTCard/NFTCard.types.ts
@@ -1,15 +1,11 @@
-import { Dispatch } from 'redux'
-import { CallHistoryMethodAction } from 'connected-react-router'
 import { Order } from '../../modules/order/types'
 import { NFT } from '../../modules/nft/types'
 
 export type Props = {
   nft: NFT
   order?: Order
-  onNavigate: (path: string) => void
 }
 
 export type MapStateProps = Pick<Props, 'order'>
-export type MapDispatchProps = Pick<Props, 'onNavigate'>
+export type MapDispatchProps = {}
 export type OwnProps = Pick<Props, 'nft' | 'order'>
-export type MapDispatch = Dispatch<CallHistoryMethodAction>
--- webapp/src/themes/index.ts
+++ webapp/src/themes/index.ts
@@ -7,4 +7,5 @@ import 'decentraland-ui/lib/dark-theme.css'
 // Overrides
 import './components/Button.css'
 import './components/Card.css'
+import './components/Navigation.css'
 import './components/Field.css'
