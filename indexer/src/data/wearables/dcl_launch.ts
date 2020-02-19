78b10ed28180d562224ec44ed2179f918173efff
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-02-19 02:47:59 -0300

    feat: add the rest of the collections

---
+++ indexer/src/data/wearables/dcl_launch.ts
@@ -0,0 +1,132 @@
+import { Wearable } from './Wearable'
+
+export let dcl_launch: Wearable[] = [
+  new Wearable(
+    'dcl_earrings_earring',
+    'Decentraland Logo Earrings',
+    '',
+    'earring',
+    'swanky',
+    ['BaseFemale', 'BaseMale']
+  ),
+  new Wearable('dcl_mana_earring', 'MANA Earrings', '', 'earring', 'epic', [
+    'BaseFemale',
+    'BaseMale'
+  ]),
+  new Wearable('mana_eyewear', 'MANA Eyewear', '', 'eyewear', 'legendary', [
+    'BaseFemale',
+    'BaseMale'
+  ]),
+  new Wearable(
+    'razor_blade_eyewear',
+    'Razor Blade Eyewear',
+    '',
+    'eyewear',
+    'mythic',
+    ['BaseFemale', 'BaseMale']
+  ),
+  new Wearable('m_barbarian_feet', 'Barbarian Shoes', '', 'feet', 'mythic', [
+    'BaseMale'
+  ]),
+  new Wearable('razor_blade_feet', 'Razor Blade Shoes', '', 'feet', 'mythic', [
+    'BaseMale',
+    'BaseFemale'
+  ]),
+  new Wearable('colorful_hat_hat', 'Coloful Hat', '', 'hat', 'epic', [
+    'BaseMale',
+    'BaseFemale'
+  ]),
+  new Wearable('dcl_hat_hat', 'Decentraland Hat', '', 'hat', 'swanky', [
+    'BaseMale',
+    'BaseFemale'
+  ]),
+  new Wearable(
+    'm_barbarian_helmet_hat',
+    'Barbarian Helmet',
+    '',
+    'hat',
+    'mythic',
+    ['BaseMale']
+  ),
+  new Wearable('mana_hat', 'MANA Hat', '', 'hat', 'swanky', [
+    'BaseMale',
+    'BaseFemale'
+  ]),
+  new Wearable('umbrella_hat', 'Umbrella Hat', '', 'hat', 'epic', [
+    'BaseMale',
+    'BaseFemale'
+  ]),
+  new Wearable(
+    'm_barbarian_lower_body',
+    'Barbarian Trousers',
+    '',
+    'lower_body',
+    'mythic',
+    ['BaseMale']
+  ),
+  new Wearable(
+    'razor_blade_lower_body',
+    'Razor Blade Trousers',
+    '',
+    'lower_body',
+    'mythic',
+    ['BaseFemale', 'BaseMale']
+  ),
+  new Wearable(
+    'dcl_hoodie_upper_body',
+    'Decentraland Hoodie',
+    '',
+    'upper_body',
+    'swanky',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'dcl_tshirt_upper_body',
+    'Decentraland T-Shirt',
+    '',
+    'upper_body',
+    'uncommon',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'launch_tshirt_upper_body',
+    'Decentraland Launch T-Shirt',
+    '',
+    'upper_body',
+    'swanky',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'm_barbarian_upper_body',
+    'Barbarian Chest Piece',
+    '',
+    'upper_body',
+    'mythic',
+    ['BaseMale']
+  ),
+  new Wearable(
+    'mana_hoodie_upper_body',
+    'MANA Hoodie',
+    '',
+    'upper_body',
+    'swanky',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mana_tshirt_upper_body',
+    'MANA T-Shirt',
+    '',
+    'upper_body',
+    'swanky',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'razor_blade_upper_body',
+    'Razor Blade Jacket',
+    '',
+    'upper_body',
+    'mythic',
+    ['BaseMale', 'BaseFemale']
+  )
+]
+
---
+++ indexer/src/data/wearables/mch_collection.ts
@@ -0,0 +1,125 @@
+import { Wearable } from './Wearable'
+
+export let mch_collection: Wearable[] = [
+  new Wearable(
+    'mch_caocao_upper_body',
+    'Caocao T-Shirt',
+    '',
+    'upper_body',
+    'mythic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_cleopatra_upper_body',
+    'Cleopatra T-Shirt',
+    '',
+    'upper_body',
+    'mythic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_enemy_upper_body',
+    'MCH Rare Enemy T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_fukuzawa_upper_body',
+    'Fukuzawa T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_gennnaihiraga_upper_body',
+    'Hiraga Gennai T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_georgewashington_upper_body',
+    'George Washington T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_huangzhong_upper_body',
+    'Huang Zhong T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_kingarthur_upper_body',
+    'King Arthur T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_leodavinci_upper_body',
+    'Leonardo Da Vinci T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_napoleon_upper_body',
+    'Napoleon T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_nightingale_upper_body',
+    'Nightingale T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_nobunagaoda_upper_body',
+    'Nobunaga Oda T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_pascal_upper_body',
+    'Pascal T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_spartacs_upper_body',
+    'Spartacs T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'mch_wrightbrother_upper_body',
+    'Wright Brothers T-Shirt',
+    '',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  )
+]
+
