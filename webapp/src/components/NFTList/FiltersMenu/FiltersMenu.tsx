3a6e5a14c9c2ccca6c298a1cb19852550dca320c
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-06-22 20:05:08 -0300

    refactor: remove unnecessary useMemo dependencies

--- webapp/src/components/NFTList/FiltersMenu/FiltersMenu.tsx
+++ webapp/src/components/NFTList/FiltersMenu/FiltersMenu.tsx
@@ -21,17 +21,17 @@ import './FiltersMenu.css'
 export const ALL_COLLECTIONS_FILTER_OPTION = 'all'
 
 // Default values
-const RARITY_FILTER_OPTIONS = Object.values(WearableRarity)
-  .filter(x => x !== WearableRarity.COMMON && x !== WearableRarity.UNIQUE)
-  .reverse()
-
-const GENDER_FILTER_OPTIONS = Object.values(WearableGender)
-
 const COLLECTION_FILTER_OPTIONS = Object.keys(contractAddresses).filter(
   (contractName: string) =>
     contractCategories[contractAddresses[contractName as ContractName]] ===
     NFTCategory.WEARABLE
 ) as ContractName[]
+
+const RARITY_FILTER_OPTIONS = Object.values(WearableRarity)
+  .filter(x => x !== WearableRarity.COMMON && x !== WearableRarity.UNIQUE)
+  .reverse()
+
+const GENDER_FILTER_OPTIONS = Object.values(WearableGender)
 
 const FiltersMenu = (props: Props) => {
   const {
@@ -54,26 +54,21 @@ const FiltersMenu = (props: Props) => {
         text: contractSymbols[contractAddresses[collection]]
       }))
     ]
-  }, [
-    ALL_COLLECTIONS_FILTER_OPTION,
-    COLLECTION_FILTER_OPTIONS,
-    contractSymbols,
-    contractAddresses
-  ])
+  }, [])
 
   const rarityOptions = useMemo(() => {
     return RARITY_FILTER_OPTIONS.map(rarity => ({
       value: rarity,
       text: t(`wearable.rarity.${rarity}`)
     }))
-  }, [RARITY_FILTER_OPTIONS])
+  }, [])
 
   const genderOptions = useMemo(() => {
     return GENDER_FILTER_OPTIONS.map(gender => ({
       value: gender,
       text: t(`wearable.body_shape.${gender}`)
     }))
-  }, [GENDER_FILTER_OPTIONS])
+  }, [])
 
   return (
     <>
