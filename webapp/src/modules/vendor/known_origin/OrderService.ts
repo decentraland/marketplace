fc8f4f05c119e538e363a332d081d339b20c4665
Nicolás Santángelo <santangelonicolas@gmail.com>
2020-08-12 17:53:04 -0300

    fix: correct buy adapter method

--- webapp/src/modules/vendor/known_origin/OrderService.ts
+++ webapp/src/modules/vendor/known_origin/OrderService.ts
@@ -5,10 +5,21 @@ import { MarketplaceAdapter } from '../../../contracts/MarketplaceAdapter'
 import { ContractFactory } from '../../contract/ContractFactory'
 import { NFT } from '../../nft/types'
 import { Order } from '../../order/types'
+import { TokenConverter } from '../TokenConverter'
+import { MarketplacePrice } from '../MarketplacePrice'
+import { ContractService as DecentralandContractService } from '../decentraland/ContractService'
 import { OrderService as OrderServiceInterface } from '../services'
 import { ContractService } from './ContractService'
 
 export class OrderService implements OrderServiceInterface {
+  private tokenConverter: TokenConverter
+  private marketplacePrice: MarketplacePrice
+
+  constructor() {
+    this.tokenConverter = new TokenConverter()
+    this.marketplacePrice = new MarketplacePrice()
+  }
+
   fetchByNFT(): any {
     throw new Error('Method: `fetchByNFT` is not implemented')
   }
@@ -21,12 +32,22 @@ export class OrderService implements OrderServiceInterface {
     if (!fromAddress) {
       throw new Error('Invalid address. Wallet must be connected.')
     }
+
     // Addresses
     const assetMarketAddress: Address = Address.fromString(order.marketAddress)
+    const manaTokenAddress = Address.fromString(
+      DecentralandContractService.contractAddresses.MANAToken
+    )
     const from = Address.fromString(fromAddress)
 
     // Data
     const calldata = this.getCallData(fromAddress, nft)
+
+    // Price
+    const manaPrice = await this.tokenConverter.contractEthToMANA(
+      order.ethPrice!
+    )
+    const maxPrice = this.marketplacePrice.addMaxSlippage(manaPrice)
 
     // Contract
     const marketplaceAdapter = ContractFactory.build(
@@ -34,7 +55,13 @@ export class OrderService implements OrderServiceInterface {
       ContractService.contractAddresses.MarketplaceAdapter
     )
     return marketplaceAdapter.methods
-      .buy(assetMarketAddress, calldata, order.ethPrice!)
+      .buy(
+        assetMarketAddress,
+        calldata,
+        order.ethPrice!,
+        manaTokenAddress,
+        maxPrice
+      )
       .send({ from })
       .getTxHash()
   }
