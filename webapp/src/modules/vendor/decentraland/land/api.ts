e4cf77da21a63ab5623e8cd50d683cba47f2b58f
Juan Cazala <juancazala@gmail.com>
2021-01-07 17:45:12 -0300

    chore: remove unsused code

--- webapp/src/modules/vendor/decentraland/land/api.ts
+++ webapp/src/modules/vendor/decentraland/land/api.ts
@@ -1,15 +1,1 @@
-import { BaseAPI } from 'decentraland-dapps/dist/lib/api'
-
 export const ATLAS_URL = process.env.REACT_APP_ATLAS_URL!
-
-class LandAPI extends BaseAPI {
-  fetchParcel(x: number | string, y: number | string) {
-    return this.request('get', `/parcels/${x}/${y}`)
-  }
-
-  fetchEstate(id: string) {
-    return this.request('get', `/estates/${id}`)
-  }
-}
-
-export const landAPI = new LandAPI(ATLAS_URL)
