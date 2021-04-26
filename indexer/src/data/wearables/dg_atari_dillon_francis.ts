5a41b8fcebce1564694bd5f9c48811b3f9cfe702
Ignacio Mazzara <nachomazzara@gmail.com>
2021-04-26 13:10:15 -0300

    feat: add collection (#270)

---
+++ indexer/src/data/wearables/dg_atari_dillon_francis.ts
@@ -0,0 +1,21 @@
+import { Wearable } from './Wearable'
+
+export let dg_atari_dillon_francis: Wearable[] = [
+  new Wearable(
+    'gerald_patchwork_knit_sweater',
+    'Gerald Patchwork Knit Sweater',
+    'A cozy Barney Cools Gerald Patchwork Knit Sweater.',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  ),
+  new Wearable(
+    'gerald_striped_knit_sweater',
+    'Gerald Striped Knit Sweater',
+    'A cozy Barney Cools Gerald Striped Knit Sweater.',
+    'upper_body',
+    'epic',
+    ['BaseMale', 'BaseFemale']
+  )
+]
+
