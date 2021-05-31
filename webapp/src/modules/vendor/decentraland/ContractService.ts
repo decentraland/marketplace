19789b626353241934fa0be98cb6719aa18db5ff
Juan Cazala <juan@decentraland.org>
2021-05-31 17:54:15 -0300

    fix: use addresses from decentraland-transactions (#316)

--- webapp/src/modules/vendor/decentraland/ContractService.ts
+++ webapp/src/modules/vendor/decentraland/ContractService.ts
@@ -1,4 +1,5 @@
 import { ChainId, Network } from '@dcl/schemas'
+import { getContract, ContractName as CN } from 'decentraland-transactions'
 import {
   Contract,
   ContractService as ContractServiceInterface
@@ -19,7 +20,7 @@ const contracts = ({
   [AppNetwork.ROPSTEN]: [
     {
       name: ContractName.MANA,
-      address: '0x2a8fd99c19271f4f04b1b7b9c4f7cf264b626edb',
+      address: getContract(CN.MANAToken, ChainId.ETHEREUM_ROPSTEN).address,
       vendor: 'decentraland',
       category: null,
       network: Network.ETHEREUM,
@@ -27,7 +28,7 @@ const contracts = ({
     },
     {
       name: ContractName.MARKETPLACE,
-      address: '0x5424912699dabaa5f2998750c1c66e73d67ad219',
+      address: getContract(CN.Marketplace, ChainId.ETHEREUM_ROPSTEN).address,
       vendor: 'decentraland',
       category: null,
       network: Network.ETHEREUM,
@@ -35,7 +36,7 @@ const contracts = ({
     },
     {
       name: ContractName.BIDS,
-      address: '0x250fa138c0a994799c7a49df3097dc71e37b3d6f',
+      address: getContract(CN.Bid, ChainId.ETHEREUM_ROPSTEN).address,
       vendor: 'decentraland',
       category: null,
       network: Network.ETHEREUM,
@@ -43,7 +44,7 @@ const contracts = ({
     },
     {
       name: ContractName.MANA,
-      address: '0x882Da5967c435eA5cC6b09150d55E8304B838f45',
+      address: getContract(CN.MANAToken, ChainId.MATIC_MUMBAI).address,
       vendor: 'decentraland',
       category: null,
       network: Network.MATIC,
@@ -51,7 +52,7 @@ const contracts = ({
     },
     {
       name: ContractName.MARKETPLACE,
-      address: '0x4D005B77334E36cD53843e9bABDf10340862C7f2',
+      address: getContract(CN.Marketplace, ChainId.MATIC_MUMBAI).address,
       vendor: 'decentraland',
       category: null,
       network: Network.MATIC,
@@ -61,7 +62,7 @@ const contracts = ({
   [AppNetwork.MAINNET]: [
     {
       name: ContractName.MANA,
-      address: '0x0f5d2fb29fb7d3cfee444a200298f468908cc942',
+      address: getContract(CN.MANAToken, ChainId.ETHEREUM_MAINNET).address,
       vendor: 'decentraland',
       category: null,
       network: Network.ETHEREUM,
@@ -69,7 +70,7 @@ const contracts = ({
     },
     {
       name: ContractName.MARKETPLACE,
-      address: '0x8e5660b4ab70168b5a6feea0e0315cb49c8cd539',
+      address: getContract(CN.Marketplace, ChainId.ETHEREUM_MAINNET).address,
       vendor: 'decentraland',
       category: null,
       network: Network.ETHEREUM,
@@ -77,7 +78,7 @@ const contracts = ({
     },
     {
       name: ContractName.BIDS,
-      address: '0xe479dfd9664c693b2e2992300930b00bfde08233',
+      address: getContract(CN.Bid, ChainId.ETHEREUM_MAINNET).address,
       vendor: 'decentraland',
       category: null,
       network: Network.ETHEREUM,
@@ -85,7 +86,7 @@ const contracts = ({
     },
     {
       name: ContractName.MANA,
-      address: '0xA1c57f48F0Deb89f569dFbE6E2B7f46D33606fD4',
+      address: getContract(CN.MANAToken, ChainId.MATIC_MAINNET).address,
       vendor: 'decentraland',
       category: null,
       network: Network.MATIC,
@@ -93,7 +94,7 @@ const contracts = ({
     },
     {
       name: ContractName.MARKETPLACE,
-      address: '0x0000000000000000000000000000000000000000', // TODO: add contract address
+      address: getContract(CN.Marketplace, ChainId.MATIC_MAINNET).address,
       vendor: 'decentraland',
       category: null,
       network: Network.MATIC,
