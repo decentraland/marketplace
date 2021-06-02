f46a7791235cddcd2e9576483cc7baa7bf7cc42c
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-06-02 12:56:46 -0300

    feat: add authorization to action errors. ignore user rejected

--- webapp/src/modules/toast/sagas.ts
+++ webapp/src/modules/toast/sagas.ts
@@ -1,6 +1,7 @@
 import { all, takeEvery, put } from 'redux-saga/effects'
 import { toastSaga as baseToastSaga } from 'decentraland-dapps/dist/modules/toast/sagas'
 import { showToast } from 'decentraland-dapps/dist/modules/toast/actions'
+import { getChainConfiguration } from 'decentraland-dapps/dist/lib/chainConfiguration'
 import { getMetaTransactionFailureToast } from './toasts'
 import { TransferNFTFailureAction, TRANSFER_NFT_FAILURE } from '../nft/actions'
 import { Network } from '@dcl/schemas'
@@ -12,6 +13,12 @@ import {
   ExecuteOrderFailureAction,
   EXECUTE_ORDER_FAILURE
 } from '../order/actions'
+import {
+  GrantTokenFailureAction,
+  GRANT_TOKEN_FAILURE,
+  RevokeTokenFailureAction,
+  REVOKE_TOKEN_FAILURE
+} from 'decentraland-dapps/dist/modules/authorization/actions'
 
 export function* toastSaga() {
   yield all([baseToastSaga(), customToastSaga()])
@@ -22,6 +29,15 @@ function* customToastSaga() {
   yield takeEvery(CREATE_ORDER_FAILURE, handleNFTMetaTransactionFailure)
   yield takeEvery(EXECUTE_ORDER_FAILURE, handleNFTMetaTransactionFailure)
   yield takeEvery(CANCEL_ORDER_FAILURE, handleNFTMetaTransactionFailure)
+
+  yield takeEvery(
+    GRANT_TOKEN_FAILURE,
+    handleAuthorizationMetaTransactionFailure
+  )
+  yield takeEvery(
+    REVOKE_TOKEN_FAILURE,
+    handleAuthorizationMetaTransactionFailure
+  )
 }
 
 function* handleNFTMetaTransactionFailure(
@@ -31,9 +47,24 @@ function* handleNFTMetaTransactionFailure(
     | ExecuteOrderFailureAction
     | CancelOrderFailureAction
 ) {
-  const { nft } = action.payload
+  const { nft, error } = action.payload
 
-  if (nft.network === Network.MATIC) {
+  if (nft.network === Network.MATIC && !isUserDeniedSignature(error)) {
     yield put(showToast(getMetaTransactionFailureToast()))
   }
+}
+
+function* handleAuthorizationMetaTransactionFailure(
+  action: GrantTokenFailureAction | RevokeTokenFailureAction
+) {
+  const { authorization, error } = action.payload
+
+  const { network } = getChainConfiguration(authorization.chainId)
+  if (network === Network.MATIC && !isUserDeniedSignature(error)) {
+    yield put(showToast(getMetaTransactionFailureToast()))
+  }
+}
+
+function isUserDeniedSignature(message: string) {
+  return message.indexOf('User denied message signature') !== -1
 }
