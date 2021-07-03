f58b5e925e20ccd62ff36b522e3a6f942f73142f
Lautaro Petaccio <lausuper@gmail.com>
2021-07-03 15:42:46 -0300

    Add link to estate/district

--- webapp/src/components/NFTPage/ParcelDetail/ParcelDetail.tsx
+++ webapp/src/components/NFTPage/ParcelDetail/ParcelDetail.tsx
@@ -3,7 +3,9 @@ import { Container, Header } from 'decentraland-ui'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 
 import { buildExplorerUrl } from '../../../modules/nft/parcel/utils'
+import { NFTCategory } from '../../../modules/nft/types'
 import { getNFTName } from '../../../modules/nft/utils'
+import { locations } from '../../../modules/routing/locations'
 import { PageHeader } from '../../PageHeader'
 import { NFTImage } from '../../NFTImage'
 import { Row } from '../../Layout/Row'
@@ -19,11 +21,13 @@ import { TransactionHistory } from '../TransactionHistory'
 import { Bids } from '../Bids'
 import { Props } from './ParcelDetail.types'
 import './ParcelDetail.css'
+import { Link } from 'react-router-dom'
 
 const ParcelDetail = (props: Props) => {
   const { nft } = props
   const parcel = nft.data.parcel!
   const { x, y } = parcel
+  const isPartOfEstate = nft.category === NFTCategory.PARCEL && parcel.estate
 
   return (
     <>
@@ -58,7 +62,18 @@ const ParcelDetail = (props: Props) => {
               </div>
             </Header>
           }
-          right={<Owner nft={nft} />}
+          right={
+            isPartOfEstate ? (
+              <div className="estate-information">
+                {t('nft_page.estate_link')}{' '}
+                <Link to={locations.nft(nft.owner, parcel.estate!.tokenId)}>
+                  {parcel.estate!.name}
+                </Link>
+              </div>
+            ) : (
+              <Owner nft={nft} />
+            )
+          }
         />
         <Description text={parcel.data?.description} />
         <Row>
