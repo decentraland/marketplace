4e73c70e7445f3b7dd3ca7da2e3f05828cbc2429
Lautaro Petaccio <lausuper@gmail.com>
2021-07-09 15:15:53 -0300

    Replace tags in ParcelTags with coordinates

--- webapp/src/components/NFTCard/ParcelTags/ParcelTags.tsx
+++ webapp/src/components/NFTCard/ParcelTags/ParcelTags.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
+import classnames from 'classnames'
+import { Coordinate } from '../../Coordinate'
 import { ProximityTags } from '../ProximityTags'
 import { Props } from './ParcelTags.types'
-import './ParcelTags.css'
+import styles from './ParcelTags.module.css'
 
 const ParcelTags = (props: Props) => {
-  const { nft } = props
+  const { nft, className } = props
   const { x, y } = nft.data.parcel!
   return (
-    <div className="ParcelTags tags">
-      <div className="coords">
-        <div className="pin" />
-        {x},{y}
-      </div>
+    <div className={classnames([styles.ParcelTags, className])}>
+      <Coordinate className={styles.coordinates} x={Number(x)} y={Number(y)} />
       <ProximityTags nft={nft} />
     </div>
   )
