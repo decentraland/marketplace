48dc1cb90545db2524dbf6037ba10efba256f024
Lautaro Petaccio <lausuper@gmail.com>
2021-07-16 15:21:34 -0300

    Update rollbar and ignore it only in dev

--- webapp/src/lib/environment.ts
+++ webapp/src/lib/environment.ts
@@ -1,5 +1,3 @@
 export const environment = process.env.REACT_APP_ENVIRONMENT
 export const isDevelopment =
   window.location.hostname === 'localhost' || environment === 'development'
-export const isStaging = environment === 'staging'
-export const isProduction = environment === 'production'
