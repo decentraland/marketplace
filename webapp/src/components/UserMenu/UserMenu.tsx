86a24b3cefd5449bcd1f03ee1c32f443b77bf477
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-08-26 18:04:09 -0300

    fix: usermenu rebase

--- webapp/src/components/UserMenu/UserMenu.tsx
+++ webapp/src/components/UserMenu/UserMenu.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { UserMenuProps } from 'decentraland-ui'
 import { UserMenu as BaseUserMenu } from 'decentraland-dapps/dist/containers'
 import { IntroPopup } from '../IntroPopup'
 
-export default class UserMenu extends React.PureComponent {
+export default class UserMenu extends React.PureComponent<
+  Partial<UserMenuProps>
+> {
   render() {
     return (
       <>
