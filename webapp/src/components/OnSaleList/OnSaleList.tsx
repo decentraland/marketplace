9823054695d8888724ad118aa49b47f89740fd68
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-11-01 16:57:09 -0300

    Remove containing div

--- webapp/src/components/OnSaleList/OnSaleList.tsx
+++ webapp/src/components/OnSaleList/OnSaleList.tsx
@@ -5,23 +5,21 @@ import OnSaleListItem from './OnSaleListItem'
 
 const OnSaleList = ({ items }: Props) => {
   return (
-    <div>
-      <Table basic="very">
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Item</Table.HeaderCell>
-            <Table.HeaderCell>Type</Table.HeaderCell>
-            <Table.HeaderCell>Sale type</Table.HeaderCell>
-            <Table.HeaderCell>Sell price</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {items.map(item => (
-            <OnSaleListItem item={item} />
-          ))}
-        </Table.Body>
-      </Table>
-    </div>
+    <Table basic="very">
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Item</Table.HeaderCell>
+          <Table.HeaderCell>Type</Table.HeaderCell>
+          <Table.HeaderCell>Sale type</Table.HeaderCell>
+          <Table.HeaderCell>Sell price</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {items.map(item => (
+          <OnSaleListItem item={item} />
+        ))}
+      </Table.Body>
+    </Table>
   )
 }
 
