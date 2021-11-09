faba49f17fe5913fac5a93ddd246fd974e15c654
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-11-09 18:09:15 -0300

    WIP collections reducer

---
+++ webapp/src/modules/collection/reducer.ts
@@ -0,0 +1,55 @@
+import {
+  loadingReducer,
+  LoadingState
+} from 'decentraland-dapps/dist/modules/loading/reducer'
+import {
+  FetchCollectionsFailureAction,
+  FetchCollectionsRequestAction,
+  FetchCollectionsSuccessAction,
+  FETCH_COLLECTIONS_FAILURE,
+  FETCH_COLLECTIONS_REQUEST,
+  FETCH_COLLECTIONS_SUCCESS
+} from './actions'
+import { Collection } from './types'
+
+export type CollectionState = {
+  data: Record<string, Collection>
+  loading: LoadingState
+  error: string | null
+}
+
+const INITIAL_STATE: CollectionState = {
+  data: {},
+  loading: [],
+  error: null
+}
+
+type CollectionReducerAction =
+  | FetchCollectionsRequestAction
+  | FetchCollectionsSuccessAction
+  | FetchCollectionsFailureAction
+
+export function collectionReducer(
+  state = INITIAL_STATE,
+  action: CollectionReducerAction
+): CollectionState {
+  switch (action.type) {
+    case FETCH_COLLECTIONS_REQUEST:
+      return {
+        ...state,
+        loading: loadingReducer(state.loading, action)
+      }
+    case FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        loading: loadingReducer(state.loading, action)
+      }
+    case FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        loading: loadingReducer(state.loading, action)
+      }
+    default:
+      return state
+  }
+}
