0ac371396ff68806999c908dc662ee1d2da64fc1
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-11-09 17:16:07 -0300

    WIP Collection list component

--- webapp/src/components/CollectionList/CollectionList.tsx
+++ webapp/src/components/CollectionList/CollectionList.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Card } from 'decentraland-ui'
+import styles from './CollectionList.module.css'
+import listedSvg from '../../images/listed.svg'
+import { Props } from './CollectionList.types'
 
-const CollectionList = () => {
-  return <div>Collection List</div>
+const CollectionList = ({}: Props) => {
+  return (
+    <>
+      <Card className={styles.card} fluid>
+        <Card.Content className={styles.cardContent}>
+          <div className={styles.detailsContainer}>
+            <div className={styles.detailsLeft}>
+              <div className={styles.image}></div>
+            </div>
+            <div className={styles.detailsRight}>
+              <div className={styles.name}>Season 2</div>
+              <div className={styles.count}>4 Items</div>
+            </div>
+          </div>
+          <img className={styles.listed} src={listedSvg} alt="listed" />
+        </Card.Content>
+      </Card>
+    </>
+  )
 }
 
 export default React.memo(CollectionList)
