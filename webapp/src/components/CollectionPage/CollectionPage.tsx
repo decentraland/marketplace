5dcd911f13255848dafe69de97e8ca34ef80878d
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-11-16 17:50:18 -0300

    Collection header and buttons

--- webapp/src/components/CollectionPage/CollectionPage.tsx
+++ webapp/src/components/CollectionPage/CollectionPage.tsx
@@ -3,14 +3,59 @@ import { Navbar } from '../Navbar'
 import { Footer } from '../Footer'
 import { Navigation } from '../Navigation'
 import { NavigationTab } from '../Navigation/Navigation.types'
-import { Page } from 'decentraland-ui'
+import {
+  Back,
+  Column,
+  Narrow,
+  Page,
+  Row,
+  Section,
+  Header,
+  Badge,
+  Icon,
+  Color,
+  Button
+} from 'decentraland-ui'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
+import styles from './CollectionPage.module.css'
 
 const CollectionPage = () => {
   return (
-    <div className="AccountPage">
+    <div>
       <Navbar isFullscreen />
       <Navigation activeTab={NavigationTab.MY_STORE} />
-      <Page></Page>
+      <Page className={styles.page}>
+        <Section>
+          <Column>
+            <Back absolute />
+            <Narrow>
+              <Row stacked>
+                <Column>
+                  <Row>
+                    <Header size="large">Season 2</Header>
+                    <Badge color={Color.SUMMER_RED}>
+                      <Icon name="tag" />
+                      <span className={styles.badge}>
+                        {t('collection_page.on_sale')}
+                      </span>
+                    </Badge>
+                  </Row>
+                </Column>
+                <Column align="right">
+                  <Row align="right">
+                    <Button primary inverted compact>
+                      {t('collection_page.edit_in_builder')}
+                    </Button>
+                    <Button primary inverted compact>
+                      {t('collection_page.unlist_from_market')}
+                    </Button>
+                  </Row>
+                </Column>
+              </Row>
+            </Narrow>
+          </Column>
+        </Section>
+      </Page>
       <Footer />
     </div>
   )
