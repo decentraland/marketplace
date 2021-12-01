1a68a4fc73a04aedb27e870c676b8061b02c7231
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-12-01 15:39:00 -0300

    Remove comments and show buttons for owner

--- webapp/src/components/AssetPage/ItemDetail/ItemDetail.tsx
+++ webapp/src/components/AssetPage/ItemDetail/ItemDetail.tsx
@@ -11,26 +11,15 @@ import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Rarity } from '@dcl/schemas'
 import { formatMANA } from '../../../lib/mana'
 import { locations } from '../../../modules/routing/locations'
-// import { AssetType } from '../../../modules/asset/types'
-// import { getAssetName } from '../../../modules/asset/utils'
-// import { Row } from '../../Layout/Row'
-// import { Column } from '../../Layout/Column'
 import { AssetImage } from '../../AssetImage'
 import { PageHeader } from '../../PageHeader'
 import { Mana } from '../../Mana'
-// import { Title } from '../Title'
 import { Network } from '../Network'
 import { Description } from '../Description'
-// import { WearableRarity } from '../WearableRarity'
-// import { WearableHighlights } from '../WearableHighlights'
-// import { WearableCollection } from '../WearableCollection'
 import { Props } from './ItemDetail.types'
 import Title from '../V2/Title'
 import RarityBadge from '../V2/RarityBadge'
 import { AssetType } from '../../../modules/asset/types'
-// import IconBadge from '../V2/IconBadge'
-// import { isGender, isUnisex } from '../../../modules/nft/wearable/utils'
-// import { BodyShape } from '@dcl/schemas'
 import GenderBadge from '../V2/GenderBadge'
 import CategoryBadge from '../V2/CategoryBadge'
 import { Box } from '../../AssetBrowse/Box'
@@ -67,80 +56,12 @@ const ItemDetail = ({ item, wallet }: Props) => {
           </div>
           <div className={styles.right}>
             <Box className={styles.box}>
-              {isOwner ? (
-                !item.isOnSale ? (
-                  <>
-                    <div className={styles.stockAndNetwork}>
-                      <Stats title={t('asset_page.available')}>
-                        {item.available > 0 ? (
-                          <Header>
-                            {item.available.toLocaleString()}
-                            <span className={styles.supply}>
-                              /
-                              {Rarity.getMaxSupply(
-                                item.rarity
-                              ).toLocaleString()}
-                            </span>
-                          </Header>
-                        ) : (
-                          t('asset_page.sold_out')
-                        )}
-                      </Stats>
-                      <Network asset={item} />
-                    </div>
-                    <Button fluid>OPEN IN EDITOR</Button>
-                  </>
-                ) : (
-                  <></>
-                )
-              ) : item.isOnSale ? (
-                <>
-                  <Stats title={t('asset_page.price')}>
-                    <Mana network={item.network} withTooltip>
-                      {formatMANA(item.price)}
-                    </Mana>
-                  </Stats>
-                  <div className={styles.stockAndNetwork}>
-                    <Stats title={t('asset_page.available')}>
-                      {item.available > 0 ? (
-                        <Header>
-                          {item.available.toLocaleString()}
-                          <span className={styles.supply}>
-                            /{Rarity.getMaxSupply(item.rarity).toLocaleString()}
-                          </span>
-                        </Header>
-                      ) : (
-                        t('asset_page.sold_out')
-                      )}
-                    </Stats>
-                    <Network asset={item} />
-                  </div>
-                  {canBuy ? (
-                    <Button
-                      fluid
-                      as={Link}
-                      to={locations.buy(
-                        AssetType.ITEM,
-                        item.contractAddress,
-                        item.itemId
-                      )}
-                      primary
-                    >
-                      {t('asset_page.actions.buy')}
-                    </Button>
-                  ) : null}
-                </>
-              ) : (
-                <></>
-              )}
-              {/* <Row>
-                <Stats title={t('asset_page.price')}>
-                  <Mana network={item.network} withTooltip>
-                    {formatMANA(item.price)}
-                  </Mana>
-                </Stats>
-              </Row>
-              <Row>
+              <Stats title={t('asset_page.price')}>
+                <Mana network={item.network} withTooltip>
+                  {formatMANA(item.price)}
+                </Mana>
+              </Stats>
+              <div className={styles.stockAndNetwork}>
                 <Stats title={t('asset_page.available')}>
                   {item.available > 0 ? (
                     <Header>
@@ -154,73 +75,32 @@ const ItemDetail = ({ item, wallet }: Props) => {
                   )}
                 </Stats>
                 <Network asset={item} />
-              </Row> */}
+              </div>
+              {isOwner ? (
+                <div className={styles.ownerButtons}>
+                  <Button fluid>EDIT PRICE</Button>
+                  <Button fluid>CHANGE BENEFICIARY</Button>
+                  <Button fluid>MINT ITEM</Button>
+                </div>
+              ) : (
+                canBuy && (
+                  <Button
+                    fluid
+                    as={Link}
+                    to={locations.buy(
+                      AssetType.ITEM,
+                      item.contractAddress,
+                      item.itemId
+                    )}
+                    primary
+                  >
+                    {t('asset_page.actions.buy')}
+                  </Button>
+                )
+              )}
             </Box>
           </div>
         </div>
-
-        {/* <Row>
-          <Column></Column>
-        </Row> */}
-        {/* <Title
-          left={
-            <Header size="large">
-              <div className="text">
-                {getAssetName(item)}
-                <WearableRarity type={AssetType.ITEM} wearable={wearable} />
-              </div>
-            </Header>
-          }
-          right={<Owner asset={item} />}
-        /> */}
-        {/* <Description text={wearable.description} />
-        <Row>
-          <Column align="left" grow={true}>
-            <Network asset={item} />
-            {item.isOnSale ? (
-              <>
-                <Stats title={t('asset_page.price')}>
-                  <Mana network={item.network} withTooltip>
-                    {formatMANA(item.price)}
-                  </Mana>
-                </Stats>
-                <Stats title={t('asset_page.available')}>
-                  {item.available > 0 ? (
-                    <Header>
-                      {item.available.toLocaleString()}
-                      <span className={styles.supply}>
-                        /{Rarity.getMaxSupply(item.rarity).toLocaleString()}
-                      </span>
-                    </Header>
-                  ) : (
-                    t('asset_page.sold_out')
-                  )}
-                </Stats>
-              </>
-            ) : null}
-          </Column>
-          <Column align="right">
-            {canBuy ? (
-              <Button
-                as={Link}
-                to={locations.buy(
-                  AssetType.ITEM,
-                  item.contractAddress,
-                  item.itemId
-                )}
-                primary
-              >
-                {t('asset_page.actions.buy')}
-              </Button>
-            ) : null}
-          </Column>
-        </Row>
-        <Row>
-          <Column>
-            <WearableCollection type={AssetType.ITEM} asset={item} />
-          </Column>
-        </Row>
-        <WearableHighlights type={AssetType.ITEM} wearable={wearable} /> */}
       </Container>
     </div>
   )
