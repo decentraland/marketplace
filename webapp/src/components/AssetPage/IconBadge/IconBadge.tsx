103b57cb790d6f696c2376a547c5d4b075b1a4c3
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-12-06 12:56:35 -0300

    feat: Remove IconBadge .module css in favor of plain css

--- webapp/src/components/AssetPage/IconBadge/IconBadge.tsx
+++ webapp/src/components/AssetPage/IconBadge/IconBadge.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { Props } from './IconBadge.types'
 import classNames from 'classnames'
-
-import styles from './IconBadge.module.css'
+import { Props } from './IconBadge.types'
 import './IconBadge.css'
 
 const IconBadge = ({ icon, text, onClick }: Props) => {
   return (
-    <div className={classNames(styles.badge, 'IconBadge')} onClick={onClick}>
-      <span className={classNames(styles.icon, icon)} />
-      <span className={styles.text}>{text}</span>
+    <div className="IconBadge" onClick={onClick}>
+      <span className={classNames('icon', icon)} />
+      <span className="text">{text}</span>
     </div>
   )
 }
