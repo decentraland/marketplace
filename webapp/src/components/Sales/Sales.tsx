62a093e0ce6818ac9de42f73e4964a89d5efc651
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-12-20 13:59:18 -0300

    feat: Add simple stats and stylings

--- webapp/src/components/Sales/Sales.tsx
+++ webapp/src/components/Sales/Sales.tsx
@@ -18,6 +18,29 @@ const Sales = () => {
           onChange={(_, data) => setCurrent(data.value as any)}
         />
       </div>
+      <div className="simple-stats">
+        <SimpleStat subtitle="Total sales" value="4" />
+        <SimpleStat subtitle="Ethereum earnings" value="11,235" />
+        <SimpleStat subtitle="Polygon earnings" value="30,600" />
+      </div>
+    </div>
+  )
+}
+
+const SimpleStat = ({
+  value,
+  subtitle
+}: {
+  value: string
+  subtitle: string
+}) => {
+  return (
+    <div className="simple-stat">
+      <div className="simple-stat-icon"></div>
+      <div className="simple-stat-details">
+        <div className="simple-stat-value">{value}</div>
+        <div className="simple-stat-subtitle">{subtitle}</div>
+      </div>
     </div>
   )
 }
