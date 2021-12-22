0e3e855b64c255a1019cf99866d61aedb12340b0
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-12-22 14:39:13 -0300

    feat: Implement asset cell in onsale list element

--- webapp/src/components/OnSaleList/OnSaleListElement/OnSaleListElement.tsx
+++ webapp/src/components/OnSaleList/OnSaleListElement/OnSaleListElement.tsx
@@ -1,50 +1,17 @@
 import React from 'react'
 import { Table } from 'decentraland-ui'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
-import { NFTCategory } from '@dcl/schemas'
-import { Link } from 'react-router-dom'
 import { Mana } from '../../Mana'
 import { formatMANA } from '../../../lib/mana'
 import { Props } from './OnSaleListElement.types'
-import { AssetImage } from '../../AssetImage'
-import { locations } from '../../../modules/routing/locations'
-import styles from './OnSaleListElement.module.css'
+import AssetCell from '../AssetCell'
 
 const OnSaleListElement = ({ nft, item, order }: Props) => {
   const category = item?.category || nft!.category
 
-  let subtitle: string | undefined
-
-  switch (category) {
-    case NFTCategory.ESTATE:
-      subtitle = t('global.parcel_count', {
-        count: nft!.data.estate!.parcels.length
-      })
-      break
-    case NFTCategory.PARCEL:
-      const { x, y } = nft!.data.parcel!
-      subtitle = `${x},${y}`
-  }
-
-  const link = item
-    ? locations.item(item.contractAddress, item.itemId)
-    : locations.nft(nft!.contractAddress, nft!.tokenId)
-
   return (
     <Table.Row>
-      <Table.Cell>
-        <Link to={link}>
-          <div className={styles.firstCell}>
-            <div className={styles.imageContainer}>
-              <AssetImage asset={item || nft!} isSmall />
-            </div>
-            <div>
-              <div className={styles.title}>{item?.name || nft!.name}</div>
-              {subtitle && <div>{subtitle}</div>}
-            </div>
-          </div>
-        </Link>
-      </Table.Cell>
+      <AssetCell asset={item || nft!} />
       <Table.Cell>{t(`global.${category}`)}</Table.Cell>
       <Table.Cell>{t(`global.${item ? 'primary' : 'secondary'}`)}</Table.Cell>
       <Table.Cell>
