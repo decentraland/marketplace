514a2452175e0c44feaff6e073b10e0d16dd42fe
Juan Cazala <juancazala@gmail.com>
2021-12-22 16:58:05 -0300

    chore: remove comments from docs

--- webapp/src/components/AssetPage/ErrorBoundary/ErrorBoundary.tsx
+++ webapp/src/components/AssetPage/ErrorBoundary/ErrorBoundary.tsx
@@ -10,18 +10,15 @@ export default class ErrorBoundary extends React.Component<Props, State> {
   }
 
   static getDerivedStateFromError(_error: Error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true }
   }
 
   componentDidCatch(error: Error, errorInfo: any) {
-    // You can also log the error to an error reporting service
     console.error(error, errorInfo)
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return (
         <Center>
           <div className="secondary-text">{t('error_boundary.message')}</div>
