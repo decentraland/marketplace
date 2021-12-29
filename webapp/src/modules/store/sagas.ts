b689e67f18abca54e232536f612f36bbdd3a7c53
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-12-29 17:20:30 -0300

    feat: Submit image to catalyst

--- webapp/src/modules/store/sagas.ts
+++ webapp/src/modules/store/sagas.ts
@@ -1,6 +1,11 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects'
 import { AuthIdentity, Authenticator } from 'dcl-crypto'
-import { CatalystClient, DeploymentPreparationData } from 'dcl-catalyst-client'
+import {
+  BuildEntityOptions,
+  BuildEntityWithoutFilesOptions,
+  CatalystClient,
+  DeploymentPreparationData
+} from 'dcl-catalyst-client'
 import { getIdentity } from '../identity/utils'
 import {
   updateStoreFailure,
@@ -11,6 +16,7 @@ import {
 import { Store as CatalystStore } from '@dcl/schemas'
 import { getAddress } from '../wallet/selectors'
 import { Store } from './types'
+import { getData as getStoresByOwner } from './selectors'
 
 export function* storeSaga() {
   yield takeLatest(UPDATE_STORE_REQUEST, handleUpdateStoreRequest)
@@ -21,41 +27,41 @@ function* handleUpdateStoreRequest(action: UpdateStoreRequestAction) {
     const { store } = action.payload
 
     const identity: AuthIdentity = yield call(getIdentity)
-
     const address: string = (yield select(getAddress))!
-
-    const client: CatalystClient = new CatalystClient(
-      process.env.REACT_APP_PEER_URL!,
-      'Market'
-    )
-
-    const links: CatalystStore['links'] = []
-
-    if (store.website) {
-      links.push({ name: 'website', url: store.website })
-    }
-
-    if (store.facebook) {
-      links.push({ name: 'facebook', url: store.facebook })
-    }
-
-    if (store.twitter) {
-      links.push({ name: 'twitter', url: store.twitter })
-    }
-
-    if (store.discord) {
-      links.push({ name: 'discord', url: store.discord })
-    }
-
+    const peerUrl = process.env.REACT_APP_PEER_URL!
+    const client: CatalystClient = new CatalystClient(peerUrl, 'Market')
     const metadata: CatalystStore = toCatalystStore(store, address)
 
-    const entity: DeploymentPreparationData = yield client.buildEntity({
+    const storesByOwner: ReturnType<typeof getStoresByOwner> = yield select(
+      getStoresByOwner
+    )
+
+    const hasDifferentCover = storesByOwner[address]?.cover !== store.cover
+
+    let entity: DeploymentPreparationData
+
+    const baseOptions: BuildEntityWithoutFilesOptions = {
       type: 'store' as any,
-      pointers: [metadata.id],
-      files: new Map(),
+      pointers: [address],
       metadata,
       timestamp: Date.now()
-    })
+    }
+
+    if (hasDifferentCover) {
+      const files = new Map<string, Buffer>()
+
+      if (store.cover && store.coverName) {
+        const response: Response = yield fetch(store.cover)
+        const arrayBuffer: Blob = yield response.arrayBuffer()
+        files.set(`cover/${store.coverName}`, Buffer.from(arrayBuffer))
+      }
+
+      const optionsWithFiles: BuildEntityOptions = { ...baseOptions, files }
+
+      entity = yield client.buildEntity(optionsWithFiles)
+    } else {
+      entity = yield client.buildEntityWithoutNewFiles(baseOptions)
+    }
 
     const authChain = Authenticator.signPayload(identity, entity.entityId)
 
@@ -86,10 +92,16 @@ const toCatalystStore = (store: Store, address: string): CatalystStore => {
     links.push({ name: 'discord', url: store.discord })
   }
 
+  let images: CatalystStore['images'] = []
+
+  if (store.cover && store.coverName) {
+    images.push({ name: 'cover', file: `cover/${store.coverName}` })
+  }
+
   return {
     id: `urn:decentraland:marketplace:store:${address}`,
     description: store.description,
-    images: [],
+    images,
     links,
     owner: address,
     version: 1
--- webapp/src/modules/store/utils.ts
+++ webapp/src/modules/store/utils.ts
@@ -3,6 +3,7 @@ import { Store } from './types'
 export const getEmptyLocalStore = (): Store => ({
   owner: '',
   cover: '',
+  coverName: '',
   description: '',
   website: '',
   facebook: '',
