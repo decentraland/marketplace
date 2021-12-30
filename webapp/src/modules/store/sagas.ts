68a28da63a649f6ed8675c0989a5563e6b0bcdd5
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2021-12-30 11:43:15 -0300

    refactor: Moved toStore and toCatalystStore to utils

--- webapp/src/modules/store/sagas.ts
+++ webapp/src/modules/store/sagas.ts
@@ -1,5 +1,6 @@
 import { call, put, select, takeLatest } from 'redux-saga/effects'
 import { AuthIdentity, Authenticator } from 'dcl-crypto'
+import { Store as CatalystStore } from '@dcl/schemas'
 import {
   BuildEntityOptions,
   BuildEntityWithoutFilesOptions,
@@ -17,69 +18,27 @@ import {
   updateStoreSuccess,
   UPDATE_STORE_REQUEST
 } from './actions'
-import { Store as CatalystStore } from '@dcl/schemas'
 import { getAddress } from '../wallet/selectors'
-import { Store } from './types'
 import { getData as getStoresByOwner } from './selectors'
-import { peerUrl } from '../../lib/environment'
+import { toCatalystStore, toStore } from './utils'
 
 export function* storeSaga(client: CatalystClient) {
   yield takeLatest(FETCH_STORE_REQUEST, handleFetchStoreRequest)
   yield takeLatest(UPDATE_STORE_REQUEST, handleUpdateStoreRequest)
 
-  function* handleFetchStoreRequest(action: FetchStoreRequestAction) {
+  function* handleFetchStoreRequest({
+    payload: { address }
+  }: FetchStoreRequestAction) {
     try {
-      const address = action.payload.address
       const result = ((yield client.fetchEntitiesByPointers('store' as any, [
         address
       ])) as unknown) as any[]
 
       if (result.length === 0) {
-        yield put(fetchStoreSuccess())
-        return
+        yield put(fetchStoreFailure('Store not found'))
+      } else {
+        yield put(fetchStoreSuccess(toStore(result[0])))
       }
-
-      const catalystStoreEntity = result[0]
-      const catalystStoreMetadata: CatalystStore = catalystStoreEntity.metadata
-
-      let cover: string = ''
-      let coverName: string = ''
-
-      const metadataCoverImage = catalystStoreMetadata.images.find(
-        image => image.name === 'cover'
-      )
-
-      if (metadataCoverImage) {
-        const contentCoverImageHash = catalystStoreEntity.content.find(
-          (cont: any) => cont.file === metadataCoverImage.file
-        )
-
-        if (contentCoverImageHash) {
-          cover = `${peerUrl}/content/contents/${contentCoverImageHash.hash}`
-          coverName = contentCoverImageHash.file
-        }
-      }
-
-      const store: Store = {
-        cover,
-        coverName,
-        description: catalystStoreMetadata.description,
-        website:
-          catalystStoreMetadata.links.find(link => link.name === 'website')
-            ?.url || '',
-        facebook:
-          catalystStoreMetadata.links.find(link => link.name === 'facebook')
-            ?.url || '',
-        twitter:
-          catalystStoreMetadata.links.find(link => link.name === 'twitter')
-            ?.url || '',
-        discord:
-          catalystStoreMetadata.links.find(link => link.name === 'discord')
-            ?.url || '',
-        owner: catalystStoreMetadata.owner
-      }
-
-      yield put(fetchStoreSuccess(store))
     } catch (e) {
       yield put(fetchStoreFailure(e.message))
     }
@@ -141,48 +100,5 @@ export function* storeSaga(client: CatalystClient) {
     } catch (e) {
       yield put(updateStoreFailure(e.message))
     }
-  }
-}
-
-const toCatalystStore = (
-  store: Store,
-  address: string,
-  hasDifferentCover: boolean
-): CatalystStore => {
-  const links: CatalystStore['links'] = []
-
-  if (store.website) {
-    links.push({ name: 'website', url: store.website })
-  }
-
-  if (store.facebook) {
-    links.push({ name: 'facebook', url: store.facebook })
-  }
-
-  if (store.twitter) {
-    links.push({ name: 'twitter', url: store.twitter })
-  }
-
-  if (store.discord) {
-    links.push({ name: 'discord', url: store.discord })
-  }
-
-  let images: CatalystStore['images'] = []
-
-  if (store.cover && store.coverName) {
-    if (hasDifferentCover) {
-      images.push({ name: 'cover', file: `cover/${store.coverName}` })
-    } else {
-      images.push({ name: 'cover', file: store.coverName })
-    }
-  }
-
-  return {
-    id: `urn:decentraland:marketplace:store:${address}`,
-    description: store.description,
-    images,
-    links,
-    owner: address,
-    version: 1
   }
 }
--- webapp/src/modules/store/utils.ts
+++ webapp/src/modules/store/utils.ts
@@ -1,4 +1,6 @@
 import { Store } from './types'
+import { Store as CatalystStore } from '@dcl/schemas'
+import { peerUrl } from '../../lib/environment'
 
 export const getEmptyLocalStore = (): Store => ({
   owner: '',
@@ -10,3 +12,91 @@ export const getEmptyLocalStore = (): Store => ({
   twitter: '',
   discord: ''
 })
+
+export const toStore = (entity: any): Store => {
+  const metadata: CatalystStore = entity.metadata
+
+  const [cover, coverName] = getCoverAndName()
+
+  return {
+    cover,
+    coverName,
+    description: metadata.description,
+    website: getLink('website'),
+    facebook: getLink('facebook'),
+    twitter: getLink('twitter'),
+    discord: getLink('discord'),
+    owner: metadata.owner
+  }
+
+  function getCoverAndName(): [string, string] {
+    const image = metadata.images.find(image => image.name === 'cover')
+
+    if (image) {
+      const content = entity.content.find(
+        (cont: any) => cont.file === image.file
+      )
+
+      if (content) {
+        return [`${peerUrl}/content/contents/${content.hash}`, content.file]
+      }
+    }
+
+    return ['', '']
+  }
+
+  function getLink(name: string) {
+    return metadata.links.find(link => link.name === name)?.url || ''
+  }
+}
+
+export const toCatalystStore = (
+  store: Store,
+  address: string,
+  hasDifferentCover: boolean
+): CatalystStore => {
+  return {
+    id: `urn:decentraland:marketplace:store:${address}`,
+    description: store.description,
+    images: getImages(),
+    links: getLinks(),
+    owner: address,
+    version: 1
+  }
+
+  function getLinks() {
+    const links: CatalystStore['links'] = []
+
+    if (store.website) {
+      links.push({ name: 'website', url: store.website })
+    }
+
+    if (store.facebook) {
+      links.push({ name: 'facebook', url: store.facebook })
+    }
+
+    if (store.twitter) {
+      links.push({ name: 'twitter', url: store.twitter })
+    }
+
+    if (store.discord) {
+      links.push({ name: 'discord', url: store.discord })
+    }
+
+    return links
+  }
+
+  function getImages() {
+    let images: CatalystStore['images'] = []
+
+    if (store.cover && store.coverName) {
+      if (hasDifferentCover) {
+        images.push({ name: 'cover', file: `cover/${store.coverName}` })
+      } else {
+        images.push({ name: 'cover', file: store.coverName })
+      }
+    }
+
+    return images
+  }
+}
