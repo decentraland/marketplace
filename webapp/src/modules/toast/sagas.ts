832a98a9a7dace1e1b313574799a8721b01c1b23
Juan Cazala <juan@decentraland.org>
2022-01-24 15:57:39 -0300

    feat: use meta-transaction error toasts from dcl-dapps (#538)

--- webapp/src/modules/toast/sagas.ts
+++ webapp/src/modules/toast/sagas.ts
@@ -1,33 +1,7 @@
 import { all, takeEvery, put } from 'redux-saga/effects'
-import { Network } from '@dcl/schemas'
-import { ErrorCode } from 'decentraland-transactions'
 import { toastSaga as baseToastSaga } from 'decentraland-dapps/dist/modules/toast/sagas'
 import { showToast } from 'decentraland-dapps/dist/modules/toast/actions'
-import {
-  GrantTokenFailureAction,
-  GRANT_TOKEN_FAILURE,
-  RevokeTokenFailureAction,
-  REVOKE_TOKEN_FAILURE
-} from 'decentraland-dapps/dist/modules/authorization/actions'
-import { getChainConfiguration } from 'decentraland-dapps/dist/lib/chainConfiguration'
-import { TransferNFTFailureAction, TRANSFER_NFT_FAILURE } from '../nft/actions'
-import {
-  CancelOrderFailureAction,
-  CANCEL_ORDER_FAILURE,
-  CreateOrderFailureAction,
-  CREATE_ORDER_FAILURE,
-  ExecuteOrderFailureAction,
-  EXECUTE_ORDER_FAILURE
-} from '../order/actions'
-import {
-  getMetaTransactionFailureToast,
-  getContractAccountFailureToast,
-  getStoreUpdateSucessToast
-} from './toasts'
-import {
-  isContractAccountError,
-  isUserDeniedSignatureError
-} from '../transaction/utils'
+import { getStoreUpdateSucessToast } from './toasts'
 import { UPDATE_STORE_SUCCESS } from '../store/actions'
 
 export function* toastSaga() {
@@ -35,66 +9,11 @@ export function* toastSaga() {
 }
 
 function* customToastSaga() {
-  yield all([successToastSagas(), failureToastSagas()])
+  yield all([successToastSagas()])
 }
 
 function* successToastSagas() {
   yield takeEvery(UPDATE_STORE_SUCCESS, handleStoreUpdateSuccess)
-}
-
-function* failureToastSagas() {
-  yield takeEvery(TRANSFER_NFT_FAILURE, handleNFTMetaTransactionFailure)
-  yield takeEvery(CREATE_ORDER_FAILURE, handleNFTMetaTransactionFailure)
-  yield takeEvery(EXECUTE_ORDER_FAILURE, handleNFTMetaTransactionFailure)
-  yield takeEvery(CANCEL_ORDER_FAILURE, handleNFTMetaTransactionFailure)
-
-  yield takeEvery(
-    GRANT_TOKEN_FAILURE,
-    handleAuthorizationMetaTransactionFailure
-  )
-  yield takeEvery(
-    REVOKE_TOKEN_FAILURE,
-    handleAuthorizationMetaTransactionFailure
-  )
-}
-
-function* handleNFTMetaTransactionFailure(
-  action:
-    | TransferNFTFailureAction
-    | CreateOrderFailureAction
-    | ExecuteOrderFailureAction
-    | CancelOrderFailureAction
-) {
-  const { nft, errorCode } = action.payload
-
-  if (nft.network === Network.MATIC) {
-    switch (errorCode) {
-      case ErrorCode.USER_DENIED:
-        // do nothing
-        break
-      case ErrorCode.CONTRACT_ACCOUNT:
-        yield put(showToast(getContractAccountFailureToast()))
-        break
-      default:
-        yield put(showToast(getMetaTransactionFailureToast()))
-    }
-  }
-}
-
-function* handleAuthorizationMetaTransactionFailure(
-  action: GrantTokenFailureAction | RevokeTokenFailureAction
-) {
-  const { authorization, error } = action.payload
-
-  const { network } = getChainConfiguration(authorization.chainId)
-
-  if (network === Network.MATIC && !isUserDeniedSignatureError(error)) {
-    if (isContractAccountError(error)) {
-      yield put(showToast(getContractAccountFailureToast()))
-    } else {
-      yield put(showToast(getMetaTransactionFailureToast()))
-    }
-  }
 }
 
 function* handleStoreUpdateSuccess() {
--- webapp/src/modules/toast/toasts.tsx
+++ webapp/src/modules/toast/toasts.tsx
@@ -1,39 +1,5 @@
 import { ToastType } from 'decentraland-ui'
-import { T, t } from 'decentraland-dapps/dist/modules/translation/utils'
-
-const DISCORD_URL = process.env.REACT_APP_DISCORD_URL
-
-export function getMetaTransactionFailureToast() {
-  return {
-    type: ToastType.ERROR,
-    title: t('toast.meta_transaction_failure.title'),
-    body: (
-      <T
-        id="toast.meta_transaction_failure.body"
-        values={{
-          br: <br />,
-          discord_link: (
-            <a href={DISCORD_URL} target="_blank" rel="noopener noreferrer">
-              Discord
-            </a>
-          )
-        }}
-      />
-    ),
-    timeout: 6000,
-    closable: true
-  }
-}
-
-export function getContractAccountFailureToast() {
-  return {
-    type: ToastType.ERROR,
-    title: t('toast.contract_account.title'),
-    body: t('toast.contract_account.body'),
-    timeout: 6000,
-    closable: true
-  }
-}
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 
 export function getStoreUpdateSucessToast() {
   return {
