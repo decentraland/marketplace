a77e0355a08a4b8db67ac6308d1044aa4ef0d8aa
Fede Ramirez <i@2fd.me>
2022-01-27 08:29:17 -0600

    feat: add sitemap proxy (#548)
    * feat: add sitemap proxy
    
    * fix: add root links

--- webapp/.ci/index.ts
+++ webapp/.ci/index.ts
@@ -1,11 +1,30 @@
+import * as pulumi from '@pulumi/pulumi'
+import * as cloudflare from '@pulumi/cloudflare'
 import { env, envTLD } from 'dcl-ops-lib/domain'
 import { buildStatic } from 'dcl-ops-lib/buildStatic'
 
 async function main() {
+  const domain = `market.decentraland.${env === 'prd' ? 'org' : envTLD}`
+
   const market = buildStatic({
-    domain: `market.decentraland.${env === 'prd' ? 'org' : envTLD}`,
+    domain,
     defaultPath: 'index.html',
   })
+
+  if (env === 'prd') {
+    const config = new pulumi.Config()
+    const hostHeaderOverride = config.requireSecret('sitemaps_target')
+    const zoneId = config.requireSecret('cloudflare_zone_id')
+    new cloudflare.PageRule(`marketplace-sitemaps-proxy`, {
+      zoneId,
+      target: domain + '/sitemap/*',
+      actions: {
+        ssl: 'flexible',
+        alwaysOnline: 'on',
+        hostHeaderOverride
+      }
+    })
+  }
 
   return {
     cloudfrontDistribution: market.cloudfrontDistribution,
