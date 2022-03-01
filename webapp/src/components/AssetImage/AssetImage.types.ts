5e2f12a3ee7d8131c00158452d0725c887b4d626
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-03-01 11:32:33 -0500

    fix: Profiles

--- webapp/src/components/AssetImage/AssetImage.types.ts
+++ webapp/src/components/AssetImage/AssetImage.types.ts
@@ -1,5 +1,5 @@
 import { Dispatch } from 'redux'
-import { Avatar } from 'decentraland-ui'
+import { Avatar } from '@dcl/schemas'
 import { Item } from '@dcl/schemas'
 import { NFT } from '../../modules/nft/types'
 
