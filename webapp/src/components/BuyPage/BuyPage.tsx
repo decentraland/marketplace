8734af3bd2aa5ec282c26efb52c47a3d677a8264
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-03-22 14:14:38 -0300

    chore: Show not found when price is 0

--- webapp/src/components/BuyPage/BuyPage.tsx
+++ webapp/src/components/BuyPage/BuyPage.tsx
@@ -13,6 +13,7 @@ import { Asset, AssetType } from '../../modules/asset/types'
 import { BuyNFTModal } from './BuyNFTModal'
 import { MintItemModal } from './MintItemModal'
 import { Props } from './BuyPage.types'
+import { NotFound } from '../AssetProviderPage/AssetProviderPage'
 import './BuyPage.css'
 
 const BuyPage = (props: Props) => {
@@ -34,11 +35,29 @@ const BuyPage = (props: Props) => {
           {wallet => (
             <AssetProviderPage type={type}>
               {(asset, order) => {
+                // Don't display the page if the cost of the item/nft is 0
+                switch (type) {
+                  case AssetType.NFT:
+                    if (order?.price === '0') {
+                      return <NotFound />
+                    }
+
+                    break
+
+                  case AssetType.ITEM:
+                    const { price } = asset as Item
+                    
+                    if (price === '0') {
+                      return <NotFound />
+                    }
+                }
+
                 const modalProps = {
                   wallet: wallet,
                   isOwner: isOwnedBy(asset, wallet),
                   hasInsufficientMANA: isInsufficientMANA(wallet, asset, order)
                 }
+
                 return type === AssetType.NFT ? (
                   <BuyNFTModal
                     nft={asset as NFT}
