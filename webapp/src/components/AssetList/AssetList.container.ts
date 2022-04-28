3a9c78a6bb633430fe46423e54c079e02c59ff7f
Braian Mellor <braianj@gmail.com>
2022-04-28 16:34:34 -0300

    feat: change way to generate urlNext by using getLocation selector

--- webapp/src/components/AssetList/AssetList.container.ts
+++ webapp/src/components/AssetList/AssetList.container.ts
@@ -17,19 +17,26 @@ import { MapStateProps, MapDispatch, MapDispatchProps } from './AssetList.types'
 import AssetList from './AssetList'
 import { FETCH_ITEMS_REQUEST } from '../../modules/item/actions'
 import { buildBrowseURL } from '../../modules/routing/utils'
+import { getLocation } from 'connected-react-router'
 
-const mapState = (state: RootState): MapStateProps => ({
-  vendor: getVendor(state),
-  assetType: getAssetType(state),
-  nfts: getNFTs(state),
-  items: getItems(state),
-  page: getPage(state),
-  count: getCount(state),
-  isLoading:
-    isLoadingType(getLoadingNFTs(state), FETCH_NFTS_REQUEST) ||
-    isLoadingType(getLoadingItems(state), FETCH_ITEMS_REQUEST),
-  urlNext: buildBrowseURL(state.router.location.pathname, { ...getCurrentBrowseOptions(state), page: (getPage(state) + 1)})  
-})
+const mapState = (state: RootState): MapStateProps => {
+  const page = getPage(state)
+  return ({
+    vendor: getVendor(state),
+    assetType: getAssetType(state),
+    nfts: getNFTs(state),
+    items: getItems(state),
+    page: page,
+    count: getCount(state),
+    isLoading:
+      isLoadingType(getLoadingNFTs(state), FETCH_NFTS_REQUEST) ||
+      isLoadingType(getLoadingItems(state), FETCH_ITEMS_REQUEST),
+    urlNext: buildBrowseURL(
+      getLocation(state).pathname, 
+      { ...getCurrentBrowseOptions(state), page: (page + 1)}
+    )  
+  })
+}
 
 const mapDispatch = (dispatch: MapDispatch): MapDispatchProps => ({
   onBrowse: options => dispatch(browse(options))
