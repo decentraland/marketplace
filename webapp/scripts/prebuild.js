76c9d1acf3228c79756438eadb94081a448766ad
Juan Cazala <juancazala@gmail.com>
2022-06-15 16:21:49 -0300

    fix: prebuild version

--- webapp/scripts/prebuild.js
+++ webapp/scripts/prebuild.js
@@ -5,21 +5,23 @@ dotenv.config()
 
 let ENV_CONTENT = {}
 
+// read files
 if (fs.existsSync('.env')) {
   Object.assign(ENV_CONTENT, dotenv.parse(fs.readFileSync('.env')))
 }
-
 const packageJson = JSON.parse(fs.readFileSync('./package.json').toString())
 const publicPackageJson = JSON.parse(
   fs.readFileSync('./public/package.json').toString()
 )
 
+// set version
 ENV_CONTENT['REACT_APP_WEBSITE_VERSION'] = packageJson.version
+publicPackageJson.version = packageJson.version
 
+// set public url
 Object.assign(ENV_CONTENT, getPublicUrls())
-
 packageJson.homepage = ENV_CONTENT['PUBLIC_URL']
-
+publicPackageJson.homepage = packageJson.homepage
 if (packageJson.homepage) {
   // github action outputs. Do not touch.
   console.log('::set-output name=public_url::' + packageJson.homepage)
@@ -28,24 +30,23 @@ if (packageJson.homepage) {
   )
 }
 
+// log stuff
 console.log('VERSIONS: ', Object.entries(ENV_CONTENT), '\n')
 
+// save files
 fs.writeFileSync(
   '.env',
   Object.entries(ENV_CONTENT)
     .map(e => e[0] + '=' + JSON.stringify(e[1]))
     .join('\n') + '\n'
 )
-
-publicPackageJson.homepage = packageJson.homepage
-publicPackageJson.version = packageJson.version
-
 fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2))
 fs.writeFileSync(
   './public/package.json',
   JSON.stringify(publicPackageJson, null, 2)
 )
 
+// public url logic
 function getPublicUrls() {
   const isStatic = !!process.env.GEN_STATIC_LOCAL
   const isCI = !!process.env.CI
