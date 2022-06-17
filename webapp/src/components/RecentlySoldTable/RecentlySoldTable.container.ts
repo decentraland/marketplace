7674b79f6de5e0311d4291a20f3965b20396c6af
Juanma Hidalgo <juanma06@gmail.com>
2022-06-17 12:43:17 +0200

    fix: fix loading state not working properly

--- webapp/src/components/RecentlySoldTable/RecentlySoldTable.container.ts
+++ webapp/src/components/RecentlySoldTable/RecentlySoldTable.container.ts
@@ -8,15 +8,17 @@ import {
 } from './RecentlySoldTable.types'
 import { getLoading } from '../../modules/sale/selectors'
 import { getSales } from '../../modules/sale/selectors'
-import { FETCH_RANKINGS_REQUEST } from '../../modules/analytics/actions'
-import AnalyticsVolumeDayData from './RecentlySoldTable'
-import { fetchSalesRequest } from '../../modules/sale/actions'
+import {
+  fetchSalesRequest,
+  FETCH_SALES_REQUEST
+} from '../../modules/sale/actions'
+import RecentlySoldTable from './RecentlySoldTable'
 
 const mapState = (state: RootState): MapStateProps => {
   const data = getSales(state)
   return {
     data,
-    isLoading: isLoadingType(getLoading(state), FETCH_RANKINGS_REQUEST)
+    isLoading: isLoadingType(getLoading(state), FETCH_SALES_REQUEST)
   }
 }
 
@@ -24,4 +26,4 @@ const mapDispatch = (dispatch: MapDispatch): MapDispatchProps => ({
   onFetchRecentSales: filters => dispatch(fetchSalesRequest(filters))
 })
 
-export default connect(mapState, mapDispatch)(AnalyticsVolumeDayData)
+export default connect(mapState, mapDispatch)(RecentlySoldTable)
