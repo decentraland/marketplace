9d8829d9ee6e1b839b9219044d2f1f74311ae278
Juanma Hidalgo <juanma06@gmail.com>
2022-07-22 17:14:08 +0200

    feat: remove extra Number that is no longer needed

--- webapp/src/components/AnalyticsVolumeDayData/utils.ts
+++ webapp/src/components/AnalyticsVolumeDayData/utils.ts
@@ -14,9 +14,9 @@ export function formatDailySales(sales: number, timeframe: AnalyticsTimeframe) {
   if (!sales) return '0'
   switch (timeframe) {
     case AnalyticsTimeframe.WEEK:
-      return `${Math.round(Number(sales / 7))}/day`
+      return `${Math.round(sales / 7)}/day`
     case AnalyticsTimeframe.MONTH:
-      return `${Math.round(Number(sales / 30))}/day`
+      return `${Math.round(sales / 30)}/day`
     default:
       return ''
   }
