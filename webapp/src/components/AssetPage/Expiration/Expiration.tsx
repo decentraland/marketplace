33b45fcb4719fe7bf742cfe0538dc70549bf1ffe
nando <24811313+fzavalia@users.noreply.github.com>
2022-08-15 13:52:39 -0300

    fix: Don't show expiration if the expiration value is not valid (#770)
    * fix: Don't show expiration if the value is not valid
    
    * chore: Move dependency

--- webapp/src/components/AssetPage/Expiration/Expiration.tsx
+++ webapp/src/components/AssetPage/Expiration/Expiration.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Icon } from 'decentraland-ui'
+import { isValid } from 'date-fns'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Props } from './Expiration.types'
 import { formatDistanceToNow } from '../../../lib/date'
-import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import styles from './Expiration.module.css'
 
 const Expiration = ({ order }: Props) => {
@@ -10,11 +11,17 @@ const Expiration = ({ order }: Props) => {
     return null
   }
 
+  // The expiration could be set to a really big and unsupported number.
+  // If the expiration has an unsupported value, don't show it.
+  if (!isValid(order.expiresAt)) {
+    return null
+  }
+
   return (
     <div className={styles.container}>
       <Icon name="clock outline" />
       {t('asset_page.expires')}{' '}
-      {formatDistanceToNow(+order.expiresAt, {
+      {formatDistanceToNow(order.expiresAt, {
         addSuffix: true
       })}
     </div>
