f75fb5a5f15a5f1bc1ba9f43fb253c6388b6d7a3
Juanma Hidalgo <juanma06@gmail.com>
2022-09-07 19:16:05 -0300

    feat: wrap the new emote categories code with a FF

--- webapp/src/components/Vendor/decentraland/NFTSections/NFTSectionsMenuItems/NFTSectionsMenuItems.types.tsx
+++ webapp/src/components/Vendor/decentraland/NFTSections/NFTSectionsMenuItems/NFTSectionsMenuItems.types.tsx
@@ -1,3 +1,7 @@
 import { Props as NFTSectionsProps } from '../NFTSections.types'
 
-export type Props = NFTSectionsProps
+export type Props = NFTSectionsProps & {
+  areEmoteCategoriesEnabled: boolean
+}
+
+export type MapStateProps = Pick<Props, 'areEmoteCategoriesEnabled'>
--- webapp/src/modules/features/types.ts
+++ webapp/src/modules/features/types.ts
@@ -1,5 +1,6 @@
 export enum FeatureName {
   MAINTENANCE = 'maintenance',
   RANKINGS = 'rankings_variant',
+  EMOTE_CATEGORIES = 'emote-categories',
   RENTALS = 'rentals'
 }
