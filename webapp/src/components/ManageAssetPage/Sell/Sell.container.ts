d4d5693796463845b3ba4f95c38c699435b9d20e
Nando <24811313+fzavalia@users.noreply.github.com>
2022-09-07 21:06:53 -0300

    feat: Sell component opens a modal on list for sale

---
+++ webapp/src/components/ManageAssetPage/Sell/Sell.container.ts
@@ -0,0 +1,21 @@
+import { Dispatch } from 'redux'
+import { connect } from 'react-redux'
+import { openModal } from 'decentraland-dapps/dist/modules/modal/actions'
+import { MapDispatchProps, MapStateProps } from './Sell.types'
+import { RootState } from '../../../modules/reducer'
+import { Metadata } from '../../Modals/TestModal/TestModal.types'
+import Sell from './Sell'
+
+const mapState = (_state: RootState): MapStateProps => ({})
+
+const mapDispatch = (dispatch: Dispatch): MapDispatchProps => ({
+  onListForSale: () =>
+    dispatch(
+      openModal('TestModal', {
+        title: 'Title',
+        subtitle: 'Subtitle'
+      } as Metadata)
+    )
+})
+
+export default connect(mapState, mapDispatch)(Sell)
