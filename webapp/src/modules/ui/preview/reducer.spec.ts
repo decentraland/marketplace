735a49cffcc74e0d1b9e741afbc980bf9a60e0a5
Gabriel Diaz <gabrieldiaz31@gmail.com>
2022-09-16 17:23:33 -0300

    feat: Update tests

--- webapp/src/modules/ui/preview/reducer.spec.ts
+++ webapp/src/modules/ui/preview/reducer.spec.ts
@@ -8,7 +8,7 @@ beforeEach(() => {
   state = { ...INITIAL_STATE }
 })
 
-describe('when reducing the successful action of setting the wearable preview', () => {
+describe('when reducing action of setting the wearable preview', () => {
   it('should return a state with the created wearable preview', () => {
     const controller = { scene: {}, emote: {} } as IPreviewController
     expect(
@@ -18,7 +18,9 @@ describe('when reducing the successful action of setting the wearable preview', 
       wearablePreviewController: controller
     })
   })
+})
 
+describe('when reducing action of setting the wearable preview to null', () => {
   it('should return a state with the unloaded wearable preview', () => {
     expect(previewReducer(state, setWearablePreviewController(null))).toEqual({
       ...INITIAL_STATE,
@@ -27,14 +29,16 @@ describe('when reducing the successful action of setting the wearable preview', 
   })
 })
 
-describe('when reducing the successful action of setting the playing emote state', () => {
+describe('when reducing action of setting the playing emote state to true', () => {
   it('should return a state with the playing emote state true', () => {
     expect(previewReducer(state, setEmotePlaying(true))).toEqual({
       ...INITIAL_STATE,
       isPlayingEmote: true
     })
   })
+})
 
+describe('when reducing action of setting the playing emote state to false', () => {
   it('should return a state with the playing emote state false', () => {
     expect(previewReducer(state, setEmotePlaying(false))).toEqual({
       ...INITIAL_STATE,
