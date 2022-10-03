e025d10f02bebef8efff5064978a0bd0d0b75287
Juanma Hidalgo <juanma06@gmail.com>
2022-10-03 17:14:31 +0200

    feat: Add emote play mode to emote card

--- webapp/src/components/AssetCard/EmoteTags/EmoteTags.tsx
+++ webapp/src/components/AssetCard/EmoteTags/EmoteTags.tsx
@@ -1,17 +1,15 @@
 import { NFTCategory } from '@dcl/schemas'
-import classnames from 'classnames'
+import classNames from 'classnames'
 import { AssetType } from '../../../modules/asset/types'
 import RarityBadge from '../../RarityBadge'
-import GenderBadge from '../../GenderBadge'
 import { Props } from './EmoteTags.types'
 import styles from './EmoteTags.module.css'
-import { Section } from '../../../modules/vendor/decentraland'
 
 const EmoteTags = (props: Props) => {
   const { nft } = props
-  const { rarity, bodyShapes } = nft.data.emote!
+  const { rarity, loop } = nft.data.emote!
   return (
-    <div className={classnames([styles.EmoteTags, 'tags'])}>
+    <div className={classNames([styles.EmoteTags, 'tags'])}>
       <RarityBadge
         size="small"
         rarity={rarity}
@@ -19,12 +17,15 @@ const EmoteTags = (props: Props) => {
         category={NFTCategory.EMOTE}
         withTooltip={false}
       />
-      <GenderBadge
-        bodyShapes={bodyShapes}
-        assetType={AssetType.NFT}
-        section={Section.EMOTES}
-        withText={false}
-      />
+
+      <div className={styles.PlayModeSmallBadge}>
+        <span
+          className={classNames(
+            styles.PlayIcon,
+            loop ? styles.PlayLoop : styles.PlayOnce
+          )}
+        ></span>
+      </div>
     </div>
   )
 }
