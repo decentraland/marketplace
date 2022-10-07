52272d51db3451de7ce593f097ee3b75521b198d
Juanma Hidalgo <juanma06@gmail.com>
2022-10-07 15:29:37 +0200

    feat: close create modal when creating a rental listing

--- webapp/src/modules/modal/sagas.ts
+++ webapp/src/modules/modal/sagas.ts
@@ -2,13 +2,17 @@ import { LOCATION_CHANGE } from 'connected-react-router'
 import { delay, put, select, takeEvery } from 'redux-saga/effects'
 import { ModalState } from 'decentraland-dapps/dist/modules/modal/reducer'
 import { getOpenModals } from 'decentraland-dapps/dist/modules/modal/selectors'
-import { CLAIM_LAND_SUCCESS, REMOVE_RENTAL_SUCCESS } from '../rental/actions'
+import {
+  CLAIM_LAND_SUCCESS,
+  CREATE_RENTAL_SUCCESS,
+  REMOVE_RENTAL_SUCCESS
+} from '../rental/actions'
 import { closeAllModals } from './actions'
 
 export function* modalSaga() {
   yield takeEvery(LOCATION_CHANGE, handleLocationChange)
   yield takeEvery(
-    [CLAIM_LAND_SUCCESS, REMOVE_RENTAL_SUCCESS],
+    [CLAIM_LAND_SUCCESS, REMOVE_RENTAL_SUCCESS, CREATE_RENTAL_SUCCESS],
     handleCloseAllModals
   )
 }
