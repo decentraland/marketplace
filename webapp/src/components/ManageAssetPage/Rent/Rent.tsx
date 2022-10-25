71a5b9d66daecb64dba81fa71c1a95e5b09b9745
Juanma Hidalgo <juanma06@gmail.com>
2022-10-25 12:32:10 +0200

    feat: remove unused import

--- webapp/src/components/ManageAssetPage/Rent/Rent.tsx
+++ webapp/src/components/ManageAssetPage/Rent/Rent.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 import classNames from 'classnames'
 import { Link } from 'react-router-dom'
 import intlFormat from 'date-fns/intlFormat'
