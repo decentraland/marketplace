4be1be1589fbb8f6c0cceef1f895d03f5ba05195
Juan Cazala <juancazala@gmail.com>
2022-11-23 14:00:36 -0300

    fix: make helper throw if fetch contracts action fails

--- webapp/src/modules/contract/utils.ts
+++ webapp/src/modules/contract/utils.ts
@@ -1,14 +1,29 @@
 import { LoadingState } from 'decentraland-dapps/dist/modules/loading/reducer'
 import { isLoadingType } from 'decentraland-dapps/dist/modules/loading/selectors'
-import { select, take } from 'redux-saga/effects'
-import { FETCH_CONTRACTS_REQUEST, FETCH_CONTRACTS_SUCCESS } from './actions'
+import { race, select, take } from 'redux-saga/effects'
+import {
+  FetchContractsFailureAction,
+  FetchContractsSuccessAction,
+  FETCH_CONTRACTS_FAILURE,
+  FETCH_CONTRACTS_REQUEST,
+  FETCH_CONTRACTS_SUCCESS
+} from './actions'
 import { getContracts, getLoading } from './selectors'
 
 export function* getOrWaitForContracts() {
   const loading: LoadingState = yield select(getLoading)
 
   if (isLoadingType(loading, FETCH_CONTRACTS_REQUEST)) {
-    yield take(FETCH_CONTRACTS_SUCCESS)
+    const result: {
+      success: FetchContractsSuccessAction
+      failure: FetchContractsFailureAction
+    } = yield race({
+      success: take(FETCH_CONTRACTS_SUCCESS),
+      failure: take(FETCH_CONTRACTS_FAILURE)
+    })
+    if (result.failure) {
+      throw new Error(`Could not load contracts`)
+    }
   }
   const contracts: ReturnType<typeof getContracts> = yield select(getContracts)
   return contracts
