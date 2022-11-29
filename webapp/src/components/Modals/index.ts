3fde6d475b23086fbb18efe4fe3bdb5dbb44b700
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2022-11-29 11:02:01 -0300

    fix: Prevent rental modals from closing (#1053)
    * fix: Prevent rental modals from closing
    
    * fix: Use function instead of undefined

--- webapp/src/components/Modals/index.ts
+++ webapp/src/components/Modals/index.ts
@@ -1,4 +1,3 @@
-export { default as TestModal } from './TestModal'
 export { ClaimLandModal } from './ClaimLandModal'
 export { RemoveRentalModal } from './RemoveRentalModal'
 export { RentalListingModal } from './RentalListingModal'
