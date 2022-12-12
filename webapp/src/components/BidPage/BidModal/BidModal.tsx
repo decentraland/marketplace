b35f4aa2f8171e36e20e1be8facb5e86da9e0b75
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2022-12-12 15:28:35 -0300

    fix: Show message on biddable rented land (#1129)
    * fix: Show message on biddable rented land
    
    * Update webapp/src/components/BidPage/BidModal/BidModal.tsx
    
    Co-authored-by: Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
    Signed-off-by: Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
    
    * Update webapp/src/components/BidPage/BidModal/BidModal.tsx
    
    Co-authored-by: Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
    Signed-off-by: Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
    
    Signed-off-by: Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
    Co-authored-by: Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>

--- webapp/src/components/BidPage/BidModal/BidModal.tsx
+++ webapp/src/components/BidPage/BidModal/BidModal.tsx
@@ -9,6 +9,11 @@ import {
 } from 'decentraland-dapps/dist/modules/authorization/types'
 import { hasAuthorization } from 'decentraland-dapps/dist/modules/authorization/utils'
 import { ChainButton } from 'decentraland-dapps/dist/containers'
+import {
+  getRentalEndDate,
+  hasRentalEnded,
+  isRentalListingExecuted
+} from '../../../modules/rental/utils'
 import { getAssetName, isOwnedBy } from '../../../modules/asset/utils'
 import { parseMANANumber } from '../../../lib/mana'
 import { AssetAction } from '../../AssetAction'
@@ -17,6 +22,7 @@ import { locations } from '../../../modules/routing/locations'
 import { useFingerprint } from '../../../modules/nft/hooks'
 import { AuthorizationModal } from '../../AuthorizationModal'
 import { getContractNames } from '../../../modules/vendor'
+import { isLand } from '../../../modules/nft/utils'
 import { ManaField } from '../../ManaField'
 import { ConfirmInputValueModal } from '../../ConfirmInputValueModal'
 import { Mana } from '../../Mana'
@@ -26,6 +32,7 @@ import './BidModal.css'
 const BidModal = (props: Props) => {
   const {
     nft,
+    rental,
     wallet,
     authorizations,
     onNavigate,
@@ -113,99 +120,116 @@ const BidModal = (props: Props) => {
 
   return (
     <AssetAction asset={nft}>
-      <Header size="large">{t('bid_page.title')}</Header>
-      <p className="subtitle">
-        <T
-          id={'bid_page.subtitle'}
-          values={{
-            name: <b className="primary-text">{getAssetName(nft)}</b>
-          }}
-        />
-      </p>
-      <Form onSubmit={handleSubmit}>
-        <div className="form-fields">
-          <ManaField
-            network={nft.network}
-            label={t('bid_page.price')}
-            placeholder={1000}
-            value={price}
-            error={price !== '' && (isInvalidPrice || hasInsufficientMANA)}
-            onChange={(_event, props) => {
-              setPrice(toFixedMANAValue(props.value))
+      <div className="bid-action">
+        <Header size="large">{t('bid_page.title')}</Header>
+        <p className="subtitle">
+          <T
+            id="bid_page.subtitle"
+            values={{
+              name: <b className="primary-text">{getAssetName(nft)}</b>
             }}
-            message={
-              hasInsufficientMANA ? t('bid_page.not_enougn_mana') : undefined
-            }
           />
-          <Field
-            network={nft.network}
-            label={t('bid_page.expiration_date')}
-            type="date"
-            value={expiresAt}
-            onChange={(_event, props) =>
-              setExpiresAt(props.value || getDefaultExpirationDate())
-            }
-            error={isInvalidDate}
-            message={isInvalidDate ? t('bid_page.invalid_date') : undefined}
-          />
-        </div>
-        <div className="buttons">
-          <Button
-            as="div"
-            disabled={isLoading || isPlacingBid}
-            onClick={() =>
-              onNavigate(locations.nft(nft.contractAddress, nft.tokenId))
-            }
-          >
-            {t('global.cancel')}
-          </Button>
-          <ChainButton
-            type="submit"
-            primary
-            loading={isPlacingBid}
-            disabled={isDisabled}
-            chainId={nft.chainId}
-          >
-            {t('bid_page.submit')}
-          </ChainButton>
-        </div>
-      </Form>
-      <AuthorizationModal
-        open={showAuthorizationModal}
-        authorization={authorization}
-        isLoading={isPlacingBid}
-        onProceed={handlePlaceBid}
-        onCancel={handleClose}
-      />
-      {showConfirmationModal ? (
-        <ConfirmInputValueModal
-          open={showConfirmationModal}
-          headerTitle={t('bid_page.confirm.title')}
-          content={
-            <>
-              <T
-                id="bid_page.confirm.create_bid_line_one"
-                values={{
-                  name: <b>{getAssetName(nft)}</b>,
-                  amount: (
-                    <Mana network={nft.network} inline>
-                      {parseMANANumber(price).toLocaleString()}
-                    </Mana>
-                  )
-                }}
-              />
-              <br />
-              <T id="bid_page.confirm.accept_bid_line_two" />
-            </>
-          }
-          onConfirm={handleConfirmBid}
-          valueToConfirm={price}
-          network={nft.network}
-          onCancel={() => setShowConfirmationModal(false)}
-          loading={isPlacingBid}
-          disabled={isPlacingBid}
+        </p>
+        {isLand(nft) &&
+        rental &&
+        isRentalListingExecuted(rental) &&
+        !hasRentalEnded(rental) ? (
+          <div className="rentalMessage">
+            <T
+              id="bid_page.rental_executed"
+              values={{
+                rental_end_date: getRentalEndDate(rental),
+                asset_type: nft.category,
+                strong: (children: any) => <strong>{children}</strong>
+              }}
+            />
+          </div>
+        ) : null}
+        <Form onSubmit={handleSubmit}>
+          <div className="form-fields">
+            <ManaField
+              network={nft.network}
+              label={t('bid_page.price')}
+              placeholder={1000}
+              value={price}
+              error={price !== '' && (isInvalidPrice || hasInsufficientMANA)}
+              onChange={(_event, props) => {
+                setPrice(toFixedMANAValue(props.value))
+              }}
+              message={
+                hasInsufficientMANA ? t('bid_page.not_enougn_mana') : undefined
+              }
+            />
+            <Field
+              network={nft.network}
+              label={t('bid_page.expiration_date')}
+              type="date"
+              value={expiresAt}
+              onChange={(_event, props) =>
+                setExpiresAt(props.value || getDefaultExpirationDate())
+              }
+              error={isInvalidDate}
+              message={isInvalidDate ? t('bid_page.invalid_date') : undefined}
+            />
+          </div>
+          <div className="buttons">
+            <Button
+              as="div"
+              disabled={isLoading || isPlacingBid}
+              onClick={() =>
+                onNavigate(locations.nft(nft.contractAddress, nft.tokenId))
+              }
+            >
+              {t('global.cancel')}
+            </Button>
+            <ChainButton
+              type="submit"
+              primary
+              loading={isPlacingBid}
+              disabled={isDisabled}
+              chainId={nft.chainId}
+            >
+              {t('bid_page.submit')}
+            </ChainButton>
+          </div>
+        </Form>
+        <AuthorizationModal
+          open={showAuthorizationModal}
+          authorization={authorization}
+          isLoading={isPlacingBid}
+          onProceed={handlePlaceBid}
+          onCancel={handleClose}
         />
-      ) : null}
+        {showConfirmationModal ? (
+          <ConfirmInputValueModal
+            open={showConfirmationModal}
+            headerTitle={t('bid_page.confirm.title')}
+            content={
+              <>
+                <T
+                  id="bid_page.confirm.create_bid_line_one"
+                  values={{
+                    name: <b>{getAssetName(nft)}</b>,
+                    amount: (
+                      <Mana network={nft.network} inline>
+                        {parseMANANumber(price).toLocaleString()}
+                      </Mana>
+                    )
+                  }}
+                />
+                <br />
+                <T id="bid_page.confirm.accept_bid_line_two" />
+              </>
+            }
+            onConfirm={handleConfirmBid}
+            valueToConfirm={price}
+            network={nft.network}
+            onCancel={() => setShowConfirmationModal(false)}
+            loading={isPlacingBid}
+            disabled={isPlacingBid}
+          />
+        ) : null}
+      </div>
     </AssetAction>
   )
 }
