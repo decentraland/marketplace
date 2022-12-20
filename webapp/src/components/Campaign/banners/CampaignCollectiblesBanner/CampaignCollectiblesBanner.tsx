6bcc5d995973e10fe1bc0fdc32bfcb327ad67c53
Juan Cazala <juancazala@gmail.com>
2022-12-20 14:01:26 -0300

    fix: homepage banner

--- webapp/src/components/Campaign/banners/CampaignCollectiblesBanner/CampaignCollectiblesBanner.tsx
+++ webapp/src/components/Campaign/banners/CampaignCollectiblesBanner/CampaignCollectiblesBanner.tsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom'
 import { Button, Desktop, Header, TabletAndBelow } from 'decentraland-ui'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { locations } from '../../../../modules/routing/locations'
+import * as decentraland from '../../../../modules/vendor/decentraland'
+import { VendorName } from '../../../../modules/vendor'
+import { SortBy } from '../../../../modules/routing/types'
+import { AssetType } from '../../../../modules/asset/types'
 
 import './CampaignCollectiblesBanner.css'
 
@@ -13,7 +17,14 @@ const CampaignCollectiblesBanner: React.FC = () => {
         <Header>{t('holiday_season_campaign.banner.collectibles.title')}</Header>
         <p>{t('holiday_season_campaign.banner.collectibles.subtitle')}</p>
       </div>
-      <Button primary as={Link} to={locations.campaign()}><Desktop>{t('holiday_season_campaign.banner.collectibles.cta')}</Desktop><TabletAndBelow>{t('holiday_season_campaign.banner.collectibles.cta_mobile')}</TabletAndBelow></Button>
+      <Button primary as={Link} to={locations.campaign({
+        section: decentraland.Section.WEARABLES,
+        vendor: VendorName.DECENTRALAND,
+        page: 1,
+        sortBy: SortBy.RECENTLY_LISTED,
+        onlyOnSale: true,
+        assetType: AssetType.ITEM
+      })}><Desktop>{t('holiday_season_campaign.banner.collectibles.cta')}</Desktop><TabletAndBelow>{t('holiday_season_campaign.banner.collectibles.cta_mobile')}</TabletAndBelow></Button>
     </div>
   )
 }
--- webapp/src/components/Campaign/banners/CampaignHomepageBanner/CampaignHomepageBanner.tsx
+++ webapp/src/components/Campaign/banners/CampaignHomepageBanner/CampaignHomepageBanner.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Desktop, Header, TabletAndBelow } from 'decentraland-ui'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { locations } from '../../../../modules/routing/locations'
+import * as decentraland from '../../../../modules/vendor/decentraland'
+import { VendorName } from '../../../../modules/vendor'
+import { SortBy } from '../../../../modules/routing/types'
+import { AssetType } from '../../../../modules/asset/types'
 
 import './CampaignHomepageBanner.css'
-import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 
 const CampaignHomepageBanner: React.FC = () => {
   return (
@@ -13,7 +17,14 @@ const CampaignHomepageBanner: React.FC = () => {
         <Header>{t('holiday_season_campaign.banner.homepage.title')}</Header>
         <p>{t('holiday_season_campaign.banner.homepage.subtitle')}</p>
       </div>
-      <Button primary as={Link} to={locations.campaign()}><Desktop>{t('holiday_season_campaign.banner.homepage.cta')}</Desktop><TabletAndBelow>{t('holiday_season_campaign.banner.homepage.cta_mobile')}</TabletAndBelow></Button>
+      <Button primary as={Link} to={locations.campaign({
+        section: decentraland.Section.WEARABLES,
+        vendor: VendorName.DECENTRALAND,
+        page: 1,
+        sortBy: SortBy.RECENTLY_LISTED,
+        onlyOnSale: true,
+        assetType: AssetType.ITEM
+      })}><Desktop>{t('holiday_season_campaign.banner.homepage.cta')}</Desktop><TabletAndBelow>{t('holiday_season_campaign.banner.homepage.cta_mobile')}</TabletAndBelow></Button>
     </div>
   )
 }
