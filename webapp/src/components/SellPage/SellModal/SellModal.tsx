f19b2e5cb4772b0e008f0843a951a8589df208de
Florencia Barreto <florbarrlad@gmail.com>
2022-12-27 11:22:36 -0300

    fix: added time to 0:00:00 to fix one more or less day depending on the timezone

--- webapp/src/components/SellPage/SellModal/SellModal.tsx
+++ webapp/src/components/SellPage/SellModal/SellModal.tsx
@@ -52,7 +52,10 @@ const SellModal = (props: Props) => {
   )
   const [expiresAt, setExpiresAt] = useState(
     isUpdate && order!.expiresAt && isValid(order!.expiresAt)
-      ? formatDate(addDays(new Date(+order!.expiresAt), 1), INPUT_FORMAT)
+      ? formatDate(
+          addDays(new Date(`${+order!.expiresAt} 00:00:00`), 1),
+          INPUT_FORMAT
+        )
       : getDefaultExpirationDate()
   )
   const [showConfirm, setShowConfirm] = useState(false)
@@ -87,7 +90,11 @@ const SellModal = (props: Props) => {
   }
 
   const handleCreateOrder = () =>
-    onCreateOrder(nft, parseMANANumber(price), new Date(expiresAt).getTime())
+    onCreateOrder(
+      nft,
+      parseMANANumber(price),
+      new Date(`${expiresAt} 00:00:00`).getTime()
+    )
 
   const handleSubmit = () => {
     if (hasAuthorization(authorizations, authorization)) {
@@ -102,7 +109,7 @@ const SellModal = (props: Props) => {
 
   const { orderService } = VendorFactory.build(nft.vendor)
 
-  const isInvalidDate = new Date(expiresAt).getTime() < Date.now()
+  const isInvalidDate = new Date(`${expiresAt} 00:00:00`).getTime() < Date.now()
   const isInvalidPrice =
     parseMANANumber(price) <= 0 || parseFloat(price) !== parseMANANumber(price)
   const isDisabled =
