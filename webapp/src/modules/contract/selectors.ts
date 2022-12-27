c4067f33b71a198796b67cfd68aee2f302ffa8d6
Nando <24811313+fzavalia@users.noreply.github.com>
2022-12-27 11:02:16 -0300

    fix: Blank page on settings while loading

--- webapp/src/modules/contract/selectors.ts
+++ webapp/src/modules/contract/selectors.ts
@@ -2,9 +2,6 @@ import { createSelectorCreator, defaultMemoize } from 'reselect'
 import isEqual from 'lodash/isEqual'
 import { Contract } from '../vendor/services'
 import { RootState } from '../reducer'
-import { LoadingState } from 'decentraland-dapps/dist/modules/loading/reducer'
-import { isLoadingType } from 'decentraland-dapps/dist/modules/loading/selectors'
-import { FETCH_CONTRACTS_REQUEST } from './actions'
 
 export const getState = (state: RootState) => state.contract
 export const getContracts = (state: RootState) => getState(state).data
@@ -15,16 +12,12 @@ export const getContract = createSelectorCreator(defaultMemoize, isEqual)<
   RootState,
   Partial<Contract>,
   Partial<Contract>,
-  ReturnType<typeof getLoading>,
   ReturnType<typeof getContracts>,
   Contract | null
 >(
   (_state: RootState, query: Partial<Contract>) => query,
-  getLoading,
   getContracts,
-  (query: Partial<Contract>, loading: LoadingState, contracts: Contract[]) => {
-    if (isLoadingType(loading, FETCH_CONTRACTS_REQUEST)) return null
-
+  (query: Partial<Contract>, contracts: Contract[]) => {
     const found = contracts.find(contract =>
       Object.keys(query).every(
         key =>
