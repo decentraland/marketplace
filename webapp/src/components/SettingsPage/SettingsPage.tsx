aafe430da7890d13f5ebc24970bdcbaef04ff53b
Nando <24811313+fzavalia@users.noreply.github.com>
2022-12-27 11:21:28 -0300

    chore: Update comment

--- webapp/src/components/SettingsPage/SettingsPage.tsx
+++ webapp/src/components/SettingsPage/SettingsPage.tsx
@@ -85,7 +85,7 @@ const SettingsPage = (props: Props) => {
   })
 
   // These contracts are defined in initialization with the redux store, so they should always be defined.
-  // If the settings is shown as blank it's because there is some sort of misconfiguration in the contracts.ts file or the redux store.
+  // If the settings is shown as blank it's because there is some sort of misconfiguration that should be addressed.
   if (
     !collectionStore ||
     !marketplaceEthereum ||
