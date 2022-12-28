31e5d3cca6ccd8712dfadb4d111b620ef034543d
Florencia Barreto <florbarrlad@gmail.com>
2022-12-28 09:46:54 -0300

    Revert "Revert "feat: updated style and added timezone, missing library update (#1166)""
    This reverts commit 5d51982f524f7ef4b8b2f955a9c8d486e6face46.

--- webapp/src/components/SellPage/SellModal/SellModal.tsx
+++ webapp/src/components/SellPage/SellModal/SellModal.tsx
@@ -52,7 +52,10 @@ const SellModal = (props: Props) => {
   )
   const [expiresAt, setExpiresAt] = useState(
     isUpdate && order!.expiresAt && isValid(order!.expiresAt)
-      ? formatDate(addDays(new Date(+order!.expiresAt), 1), INPUT_FORMAT)
+      ? formatDate(
+          addDays(new Date(`${+order!.expiresAt} 00:00:00`), 1),
+          INPUT_FORMAT
+        )
       : getDefaultExpirationDate()
   )
   const [showConfirm, setShowConfirm] = useState(false)
@@ -89,7 +92,11 @@ const SellModal = (props: Props) => {
   }
 
   const handleCreateOrder = () =>
-    onCreateOrder(nft, parseMANANumber(price), new Date(expiresAt).getTime())
+    onCreateOrder(
+      nft,
+      parseMANANumber(price),
+      new Date(`${expiresAt} 00:00:00`).getTime()
+    )
 
   const handleSubmit = () => {
     if (hasAuthorization(authorizations, authorization)) {
@@ -104,7 +111,7 @@ const SellModal = (props: Props) => {
 
   const { orderService } = VendorFactory.build(nft.vendor)
 
-  const isInvalidDate = new Date(expiresAt).getTime() < Date.now()
+  const isInvalidDate = new Date(`${expiresAt} 00:00:00`).getTime() < Date.now()
   const isInvalidPrice =
     parseMANANumber(price) <= 0 || parseFloat(price) !== parseMANANumber(price)
   const isDisabled =
