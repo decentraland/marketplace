b1af75ee17213060bbbdae3d91ba5a2cb9fa68fd
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-01-17 09:58:09 -0300

    feat: add category dropdown collapsible option (#1310)

--- webapp/src/components/Menu/DropdownMenu/DropdownMenu.tsx
+++ webapp/src/components/Menu/DropdownMenu/DropdownMenu.tsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import classNames from 'classnames'
+import React, { useCallback, useEffect, useState } from 'react'
 
 import { MenuItem } from '../../Menu/MenuItem'
 import { Props } from './DropdownMenu.types'
 
 const DropdownMenu = <T extends unknown>(props: Props<T>) => {
   const { values, currentValue, onMenuItemClick } = props
+  const [isDropdownOpen, setIsDropdownOpen] = useState(currentValue && values.includes(currentValue))
+
+  const handleMenuItemClick = useCallback((value: T) => {
+    if (value === values[0]) {
+      setIsDropdownOpen(!isDropdownOpen);
+    }
+
+    onMenuItemClick(value);
+  }, [values, isDropdownOpen, onMenuItemClick])
+
+  useEffect(() => {
+    if (!currentValue || !values.includes(currentValue)) {
+      setIsDropdownOpen(false)
+    }
+  }, [values, currentValue])
 
   return (
     <>
       <MenuItem
         value={values[0]}
         currentValue={currentValue}
-        onClick={onMenuItemClick}
+        onClick={handleMenuItemClick}
         nestedLevel={1}
         withCaret={true}
+        className={classNames({ open: isDropdownOpen })}
       />
       <ul className="submenu">
-        {values.includes(currentValue!)
+        {values.includes(currentValue!) && isDropdownOpen
           ? values
               .slice(1)
               .map((value, index) => (
@@ -24,7 +41,7 @@ const DropdownMenu = <T extends unknown>(props: Props<T>) => {
                   key={index}
                   value={value}
                   currentValue={currentValue}
-                  onClick={onMenuItemClick}
+                  onClick={handleMenuItemClick}
                   nestedLevel={2}
                 />
               ))
