117d4da7b39f610e47e0368711973dc1aa13b944
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-01-25 15:14:59 -0300

    refactor: Send Transak customizations options via constructor

--- webapp/src/lib/transak.ts
+++ webapp/src/lib/transak.ts
@@ -9,25 +9,18 @@ import { Asset } from '../modules/asset/types'
 import { isNFT } from '../modules/asset/utils'
 
 export class Transak extends BaseTransak {
-  private readonly asset: Asset
-
   constructor(asset: Asset) {
     const transakConfig: TransakConfig = {
       key: config.get('TRANSAK_KEY'),
       env: config.get('TRANSAK_ENV')
     }
-    super(transakConfig)
-    this.asset = asset
-  }
-
-  protected customizationOptions(address: string) {
-    return {
-      ...super.defaultCustomizationOptions(address),
-      contractAddress: this.asset.contractAddress,
-      tradeType: isNFT(this.asset) ? TradeType.SECONDARY : TradeType.PRIMARY,
-      tokenId: isNFT(this.asset) ? this.asset.tokenId : this.asset.itemId,
+    const providedCustomizationOptions = {
+      contractAddress: asset.contractAddress,
+      tradeType: isNFT(asset) ? TradeType.SECONDARY : TradeType.PRIMARY,
+      tokenId: isNFT(asset) ? asset.tokenId : asset.itemId,
       productsAvailed: ProductsAvailed.BUY,
       isNFT: true
     }
+    super(transakConfig, providedCustomizationOptions)
   }
 }
