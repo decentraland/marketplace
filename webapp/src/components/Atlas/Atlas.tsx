e1a300ef0ca9d9598e4288770be067fee3912c55
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-03-20 17:18:42 -0300

    fix: map view filter issues (#1471)

--- webapp/src/components/Atlas/Atlas.tsx
+++ webapp/src/components/Atlas/Atlas.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Button, Popup as UIPopup } from 'decentraland-ui'
-import { NFTCategory } from '@dcl/schemas'
+import { NFTCategory, RentalStatus } from '@dcl/schemas'
+import classNames from 'classnames'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import {
   Atlas as AtlasComponent,
@@ -39,6 +40,7 @@ const Atlas: React.FC<Props> = (props: Props) => {
   } = props
 
   const [showPopup, setShowPopup] = useState(false)
+  const [isInfoPopupOpen, setIsInfoPopupOpen] = useState(false)
   const [hoveredTile, setHoveredTile] = useState<Tile | null>(null)
   const [mouseX, setMouseX] = useState(-1)
   const [mouseY, setMouseY] = useState(-1)
@@ -97,12 +99,17 @@ const Atlas: React.FC<Props> = (props: Props) => {
 
   const userRentedTiles = useMemo(
     () =>
-      nftsOnRent
-        .map(([nft]) => nft)
-        .reduce(
-          (lands, nft) => setLand(lands, nft, Color.SUNISH),
-          new Map<string, ReturnType<Layer>>()
-        ),
+      nftsOnRent.reduce(
+        (lands, [nft, rental]) =>
+          setLand(
+            lands,
+            nft,
+            rental.status === RentalStatus.EXECUTED
+              ? Color.SUNISH
+              : Color.SUMMER_RED
+          ),
+        new Map<string, ReturnType<Layer>>()
+      ),
     [nftsOnRent, setLand]
   )
 
@@ -290,6 +297,18 @@ const Atlas: React.FC<Props> = (props: Props) => {
     }
   }, [withPopup, showPopup, mouseX, mouseY])
 
+  const handleInfoPopupOpen = useCallback(() => setIsInfoPopupOpen(true), [
+    setIsInfoPopupOpen
+  ])
+
+  const handleInfoPopupClose = useCallback(
+    (evt: React.MouseEvent) => {
+      evt.stopPropagation()
+      setIsInfoPopupOpen(false)
+    },
+    [setIsInfoPopupOpen]
+  )
+
   // layers
   const layers = [
     userLayer,
@@ -331,10 +350,14 @@ const Atlas: React.FC<Props> = (props: Props) => {
             </div>
           }
           position="top right"
+          onClose={handleInfoPopupClose}
+          onOpen={handleInfoPopupOpen}
           trigger={
             <Button
               primary
-              className="atlas-info-button"
+              className={classNames('atlas-info-button', {
+                'atlas-info-open': isInfoPopupOpen
+              })}
               aria-label="info"
               tabIndex={0}
             >
