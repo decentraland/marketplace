e48c8aa1839c6ddacb0105e5c3f5cc8886651516
Florencia Barreto <32873485+flobarreto@users.noreply.github.com>
2023-03-23 10:48:53 -0300

    Feat: mint or buy component (#1472)
    * feat: created owners table with corresponding styles
    
    * feat: added connection with nft server
    
    * feat: added sort by
    
    * feat: some fixes
    
    * feat: removed unused container
    
    * fix: pr comments
    
    * fix: imports
    
    * fix: pr commentsg
    
    * fix: removed change con dev.json
    
    * fix: pr comments
    
    * fix: changed component name
    
    * feat: create listings table component
    
    * feat: listings table with mocked data
    
    * fix: dates and sytles
    
    * feat: connected with api
    
    * feat: added empty state to tables - missing listings empty state
    
    * feat: added empty state text
    
    * fix: added name as possible filter to support marketplace subraph
    
    * fix: test
    
    * feat: updated dcl shcemas
    
    * feat: created mint or buy component
    
    * fix: pr comments

--- webapp/src/components/AssetPage/AssetPage.container.tsx
+++ webapp/src/components/AssetPage/AssetPage.container.tsx
@@ -1,10 +1,14 @@
+import { getLocation } from 'connected-react-router'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
+import { RootState } from '../../modules/reducer'
 import { goBack } from '../../modules/routing/actions'
 import AssetPage from './AssetPage'
 import { MapDispatchProps } from './AssetPage.types'
 
-const mapState = () => ({})
+const mapState = (state: RootState) => ({
+  location: getLocation(state)
+})
 
 const mapDispatch = (dispatch: Dispatch): MapDispatchProps => ({
   onBack: (location?: string) => dispatch(goBack(location))
