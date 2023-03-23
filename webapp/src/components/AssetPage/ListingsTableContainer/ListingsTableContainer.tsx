e5f3b8186c881439e1571ee4175a72f4ae0227d2
Florencia Barreto <32873485+flobarreto@users.noreply.github.com>
2023-03-23 10:48:53 -0300

    Feat: mint or buy component (#1472)
    * feat: created owners table with corresponding styles
    
    * feat: added connection with nft server
    
    * feat: added sort by
    
    * feat: some fixes
    
    * feat: removed unused container
    
    * fix: pr comments
    
    * fix: imports
    
    * fix: pr commentsg
    
    * fix: removed change con dev.json
    
    * fix: pr comments
    
    * fix: changed component name
    
    * feat: create listings table component
    
    * feat: listings table with mocked data
    
    * fix: dates and sytles
    
    * feat: connected with api
    
    * feat: added empty state to tables - missing listings empty state
    
    * feat: added empty state text
    
    * fix: added name as possible filter to support marketplace subraph
    
    * fix: test
    
    * feat: updated dcl shcemas
    
    * feat: created mint or buy component
    
    * fix: pr comments

--- webapp/src/components/AssetPage/ListingsTableContainer/ListingsTableContainer.tsx
+++ webapp/src/components/AssetPage/ListingsTableContainer/ListingsTableContainer.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Dropdown, Tabs } from 'decentraland-ui'
 import { OrderSortBy } from '@dcl/schemas'
@@ -8,92 +9,102 @@ import ListingsTable from '../ListingsTable/ListingsTable'
 import { BelowTabs, Props, SortByType } from './ListingsTableContainer.types'
 import styles from './ListingsTableContainer.module.css'
 
-const ListingsTableContainer = ({ item }: Props) => {
-  const [belowTab, setBelowTab] = useState(BelowTabs.LISTINGS)
-  const [sortBy, setSortBy] = useState<SortByType>(OrderSortBy.CHEAPEST)
+const ListingsTableContainer = forwardRef<HTMLDivElement, Props>(
+  (props, ref) => {
+    const { item } = props
+    const locations = useLocation()
+    const [belowTab, setBelowTab] = useState(BelowTabs.LISTINGS)
+    const [sortBy, setSortBy] = useState<SortByType>(OrderSortBy.CHEAPEST)
 
-  const ownerSortByOptions = [
-    {
-      text: t('owners_table.issue_number_asc'),
-      value: OrderDirection.ASC
-    },
-    {
-      text: t('owners_table.issue_number_desc'),
-      value: OrderDirection.DESC
+    const ownerSortByOptions = [
+      {
+        text: t('owners_table.issue_number_asc'),
+        value: OrderDirection.ASC
+      },
+      {
+        text: t('owners_table.issue_number_desc'),
+        value: OrderDirection.DESC
+      }
+    ]
+
+    const listingSortByOptions = [
+      {
+        text: t('listings_table.cheapest'),
+        value: OrderSortBy.CHEAPEST
+      },
+      {
+        text: t('listings_table.newest'),
+        value: OrderSortBy.RECENTLY_LISTED
+      },
+      {
+        text: t('listings_table.oldest'),
+        value: OrderSortBy.OLDEST
+      },
+      {
+        text: t('listings_table.issue_number_asc'),
+        value: OrderSortBy.ISSUED_ID_ASC
+      },
+      {
+        text: t('listings_table.issue_number_desc'),
+        value: OrderSortBy.ISSUED_ID_DESC
+      }
+    ]
+
+    useEffect(() => {
+      const params = new URLSearchParams(locations.search)
+      if (params.get('selectedTableTab') === BelowTabs.OWNERS)
+        handleTabChange(BelowTabs.OWNERS)
+    }, [locations.search])
+
+    const handleTabChange = (tab: BelowTabs) => {
+      const sortByTab =
+        tab === BelowTabs.LISTINGS ? OrderSortBy.CHEAPEST : OrderDirection.ASC
+      setBelowTab(tab)
+      setSortBy(sortByTab)
     }
-  ]
 
-  const listingSortByOptions = [
-    {
-      text: t('listings_table.cheapest'),
-      value: OrderSortBy.CHEAPEST
-    },
-    {
-      text: t('listings_table.newest'),
-      value: OrderSortBy.RECENTLY_LISTED
-    },
-    {
-      text: t('listings_table.oldest'),
-      value: OrderSortBy.OLDEST
-    },
-    {
-      text: t('listings_table.issue_number_asc'),
-      value: OrderSortBy.ISSUED_ID_ASC
-    },
-    {
-      text: t('listings_table.issue_number_desc'),
-      value: OrderSortBy.ISSUED_ID_DESC
-    }
-  ]
-
-  const handleTabChange = (tab: BelowTabs) => {
-    const sortByTab =
-      tab === BelowTabs.LISTINGS ? OrderSortBy.CHEAPEST : OrderDirection.ASC
-    setBelowTab(tab)
-    setSortBy(sortByTab)
-  }
-
-  return (
-    <div className={styles.tableContainer}>
-      <div className={styles.filtertabsContainer}>
-        <Tabs isFullscreen>
-          <Tabs.Tab
-            active={belowTab === BelowTabs.LISTINGS}
-            onClick={() => handleTabChange(BelowTabs.LISTINGS)}
-          >
-            <div className={styles.tabStyle}>
-              {t('listings_table.listings')}
-            </div>
-          </Tabs.Tab>
-          <Tabs.Tab
-            active={belowTab === BelowTabs.OWNERS}
-            onClick={() => handleTabChange(BelowTabs.OWNERS)}
-          >
-            {t('owners_table.owners')}
-          </Tabs.Tab>
-        </Tabs>
-        <Dropdown
-          direction="left"
-          className={styles.sortByDropdown}
-          value={sortBy}
-          onChange={(_event, data) => {
-            const value = data.value as SortByType
-            setSortBy(value)
-          }}
-          options={
-            belowTab === BelowTabs.LISTINGS
-              ? listingSortByOptions
-              : ownerSortByOptions
-          }
-        />
+    return (
+      <div className={styles.tableContainer} ref={ref}>
+        <div className={styles.filtertabsContainer}>
+          <Tabs isFullscreen>
+            <Tabs.Tab
+              active={belowTab === BelowTabs.LISTINGS}
+              onClick={() => handleTabChange(BelowTabs.LISTINGS)}
+            >
+              <div className={styles.tabStyle}>
+                {t('listings_table.listings')}
+              </div>
+            </Tabs.Tab>
+            <Tabs.Tab
+              active={belowTab === BelowTabs.OWNERS}
+              onClick={() => handleTabChange(BelowTabs.OWNERS)}
+            >
+              {t('owners_table.owners')}
+            </Tabs.Tab>
+          </Tabs>
+          <Dropdown
+            direction="left"
+            className={styles.sortByDropdown}
+            value={sortBy}
+            onChange={(_event, data) => {
+              const value = data.value as SortByType
+              setSortBy(value)
+            }}
+            options={
+              belowTab === BelowTabs.LISTINGS
+                ? listingSortByOptions
+                : ownerSortByOptions
+            }
+          />
+        </div>
+        {belowTab === BelowTabs.LISTINGS ? (
+          <ListingsTable asset={item} sortBy={sortBy as OrderSortBy} />
+        ) : (
+          <OwnersTable asset={item} orderDirection={sortBy as OrderDirection} />
+        )}
       </div>
-      {belowTab === BelowTabs.LISTINGS ? (
-        <ListingsTable asset={item} sortBy={sortBy as OrderSortBy} />
-      ) : (
-        <OwnersTable asset={item} orderDirection={sortBy as OrderDirection} />
-      )}
-    </div>
-  )
-}
+    )
+  }
+)
 
 export default React.memo(ListingsTableContainer)
