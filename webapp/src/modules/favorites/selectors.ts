8814ed648a91dad0251c23136782bfb32f122335
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-03-29 19:35:21 +0200

    Update webapp/src/modules/favorites/selectors.ts
    Co-authored-by: Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
    Signed-off-by: Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>

--- webapp/src/modules/favorites/selectors.ts
+++ webapp/src/modules/favorites/selectors.ts
@@ -5,7 +5,7 @@ export const getData = (state: RootState) => getState(state).data
 export const getLoading = (state: RootState) => getState(state).loading
 export const getError = (state: RootState) => getState(state).error
 
-export const getFavoritesDataByItemId = (state: RootState, itemId: string) =>
+export const getFavoritesDataByItemId = (state: RootState, itemId: string): FavoritesData | undefined =>
   getData(state)[itemId]
 
 export const getIsPickedByUser = (state: RootState, itemId: string) =>
