c6be152a3ea2ed5cca18eae38f0ecc8bfeb0c0e7
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-03-30 15:26:14 +0200

    test: Add tests for when the favorite data does not exist for the item

--- webapp/src/modules/favorites/selectors.spec.ts
+++ webapp/src/modules/favorites/selectors.spec.ts
@@ -64,15 +64,31 @@ describe('when getting the favorites data by item id', () => {
 })
 
 describe('when getting the if an item is picked by user', () => {
-  it('should return the a boolean with the value', () => {
-    expect(getIsPickedByUser(state, 'item1')).toEqual(
-      state.favorites.data.item1.pickedByUser
-    )
+  describe('and the data is already in the store', () => {
+    it('should return the a boolean with the value', () => {
+      expect(getIsPickedByUser(state, 'item1')).toEqual(
+        state.favorites.data.item1.pickedByUser
+      )
+    })
+  })
+
+  describe('and the data was not loaded to the store yet', () => {
+    it('should return false', () => {
+      expect(getIsPickedByUser(state, 'item1111')).toEqual(false)
+    })
   })
 })
 
 describe('when getting the count of favorites an item has', () => {
-  it('should return the numeric value representing the count', () => {
-    expect(getCount(state, 'item1')).toEqual(state.favorites.data.item1.count)
+  describe('and the data is already in the store', () => {
+    it('should return the numeric value representing the count', () => {
+      expect(getCount(state, 'item1')).toEqual(state.favorites.data.item1.count)
+    })
+  })
+
+  describe('and the data was not loaded to the store yet', () => {
+    it('should return the numeric value representing the count', () => {
+      expect(getCount(state, 'item1111')).toEqual(0)
+    })
   })
 })
