52e61dfd14327b3b8f54103b0afca0eb64990594
Nando <24811313+fzavalia@users.noreply.github.com>
2023-04-13 18:02:36 -0300

    feat: Add super cool animation

--- webapp/src/components/ClaimYourName/ClaimYourName.tsx
+++ webapp/src/components/ClaimYourName/ClaimYourName.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button } from 'decentraland-ui'
 import { T, t } from 'decentraland-dapps/dist/modules/translation/utils'
 import claimYourOwnNameImg from '../../images/claim-your-own-name.svg'
@@ -7,8 +7,32 @@ import { Mana } from '../Mana'
 import styles from './ClaimYourName.module.css'
 
 const ClaimYourName = () => {
+  const g = React.useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const { current } = g
+
+    const handler = (e: MouseEvent) => {
+      if (current) {
+        const x = e.clientX / window.innerWidth
+        const degrees = x * 40 - 20
+        current.style.background = `linear-gradient(${degrees}deg,#ffbc5b 0%,#ff2d55 50.52%,#c640cd 100%)`
+      }
+    }
+
+    if (current) {
+      current.addEventListener('mousemove', handler)
+    }
+
+    return () => {
+      if (current) {
+        current.removeEventListener('mousemove', handler)
+      }
+    }
+  }, [g])
+
   return (
-    <div className={styles.gradient}>
+    <div ref={g} className={styles.gradient}>
       <div className={styles.container}>
         <img
           className={styles.img}
@@ -17,16 +41,22 @@ const ClaimYourName = () => {
         ></img>
         <div>
           <h4 className={styles.title}>{t('claim_your_own_name.title')}</h4>
-          <p className={styles.text}>
+          <div className={styles.text}>
             <T
               id="claim_your_own_name.text"
               values={{
                 mana: <Mana size="small">100</Mana>
               }}
             />
-          </p>
+          </div>
         </div>
-        <Button className={styles.btn} primary fluid as={'a'} href={`${builderUrl}/names`}>
+        <Button
+          className={styles.btn}
+          primary
+          fluid
+          as={'a'}
+          href={`${builderUrl}/names`}
+        >
           {t('claim_your_own_name.btn')}
         </Button>
       </div>
