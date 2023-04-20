4e24889c72b6137e3bef7fb12acfe733ad93838b
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-04-20 12:09:47 +0200

    fix: Only the avatar (or blockie) and address of the linked profile are clickable

--- webapp/src/components/LinkedProfile/LinkedProfile.tsx
+++ webapp/src/components/LinkedProfile/LinkedProfile.tsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom'
+import classNames from 'classnames'
 import { Profile } from 'decentraland-dapps/dist/containers'
 import { locations } from '../../modules/routing/locations'
 import { Props } from './LinkedProfile.types'
+import styles from './LinkedProfile.module.css'
 
 export const LinkedProfile = (props: Props) => {
   const { address, className, browseOptions } = props
 
   return (
-    <Link className={className} to={locations.account(address, browseOptions)}>
+    <Link
+      className={classNames(styles.LinkedProfile, className)}
+      to={locations.account(address, browseOptions)}
+    >
       <Profile {...props} />
     </Link>
   )
