6c481d04cd1efdd63d329744ee4ddb1c628d1dbe
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-04-24 11:29:03 +0200

    refactor: Move the isLoading logic into a selector

--- webapp/src/components/FavoritesCounter/FavoritesCounter.container.ts
+++ webapp/src/components/FavoritesCounter/FavoritesCounter.container.ts
@@ -2,16 +2,13 @@ import { connect } from 'react-redux'
 import { Item } from '@dcl/schemas'
 import { openModal } from 'decentraland-dapps/dist/modules/modal/actions'
 import {
-  PICK_ITEM_AS_FAVORITE_REQUEST,
-  UNDO_UNPICKING_ITEM_AS_FAVORITE_REQUEST,
-  UNPICK_ITEM_AS_FAVORITE_REQUEST,
   pickItemAsFavoriteRequest,
   unpickItemAsFavoriteRequest
 } from '../../modules/favorites/actions'
 import {
   getIsPickedByUser,
   getCount,
-  getLoading
+  isPickingOrUnpicking
 } from '../../modules/favorites/selectors'
 import { RootState } from '../../modules/reducer'
 import FavoritesCounter from './FavoritesCounter'
@@ -29,14 +26,7 @@ const mapState = (state: RootState, ownProps: OwnProps): MapStateProps => {
   return {
     isPickedByUser: getIsPickedByUser(state, itemId),
     count: getCount(state, itemId),
-    isLoading: getLoading(state).some(
-      ({ type, payload }) =>
-        [
-          PICK_ITEM_AS_FAVORITE_REQUEST,
-          UNPICK_ITEM_AS_FAVORITE_REQUEST,
-          UNDO_UNPICKING_ITEM_AS_FAVORITE_REQUEST
-        ].includes(type) && payload.item.id === itemId
-    )
+    isLoading: isPickingOrUnpicking(state, itemId)
   }
 }
 
