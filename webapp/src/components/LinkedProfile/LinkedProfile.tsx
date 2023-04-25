0de637b8e3b02e83eb120fc4cc37dc59763e5897
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-04-25 18:38:09 +0200

    fix: Adapt Linked Profile to the new Profile implementation

--- webapp/src/components/LinkedProfile/LinkedProfile.tsx
+++ webapp/src/components/LinkedProfile/LinkedProfile.tsx
@@ -1,19 +1,17 @@
 import { Link } from 'react-router-dom'
-import classNames from 'classnames'
 import { Profile } from 'decentraland-dapps/dist/containers'
 import { locations } from '../../modules/routing/locations'
 import { Props } from './LinkedProfile.types'
-import styles from './LinkedProfile.module.css'
 
-export const LinkedProfile = (props: Props) => {
+export const LinkedProfile = <T extends React.ElementType>(props: Props<T>) => {
   const { address, className, browseOptions } = props
 
   return (
-    <Link
-      className={classNames(styles.LinkedProfile, className)}
+    <Profile
+      {...props}
+      className={className}
+      as={Link}
       to={locations.account(address, browseOptions)}
-    >
-      <Profile {...props} />
-    </Link>
+    />
   )
 }
--- webapp/src/components/LinkedProfile/LinkedProfile.types.ts
+++ webapp/src/components/LinkedProfile/LinkedProfile.types.ts
@@ -1,7 +1,7 @@
 import { ProfileProps } from 'decentraland-ui/dist/components/Profile/Profile'
 import { BrowseOptions } from '../../modules/routing/types'
 
-export type Props = ProfileProps & {
+export type Props<T extends React.ElementType> = ProfileProps<T> & {
   className?: string
   browseOptions?: BrowseOptions
 }
