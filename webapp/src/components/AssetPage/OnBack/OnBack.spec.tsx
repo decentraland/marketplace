bd6de5d737634941d7eb87882733c9ef03a6eff6
Florencia Barreto <florbarrlad@gmail.com>
2023-05-03 14:09:40 -0300

    fix: tests

--- webapp/src/components/AssetPage/OnBack/OnBack.spec.tsx
+++ webapp/src/components/AssetPage/OnBack/OnBack.spec.tsx
@@ -3,25 +3,20 @@ import { useMobileMediaQuery } from 'decentraland-ui/dist/components/Media'
 import { Asset } from '../../../modules/asset/types'
 import { INITIAL_STATE } from '../../../modules/favorites/reducer'
 import { renderWithProviders } from '../../../utils/test'
-import BaseDetail from './BaseDetail'
-import { Props as BaseDetailProps } from './BaseDetail.types'
+import { Props as OnBackProps } from './OnBack.types'
+import OnBack from './OnBack'
 
 jest.mock('decentraland-ui/dist/components/Media')
 
 const BASE_DETAIL_TOP_HEADER = 'top-header'
 const FAVORITES_COUNTER_TEST_ID = 'favorites-counter'
 
-function renderBaseDetail(props: Partial<BaseDetailProps> = {}) {
+function renderOnBack(props: Partial<OnBackProps> = {}) {
   return renderWithProviders(
-    <BaseDetail
-      asset={{} as Asset}
-      assetImage={undefined}
-      badges={undefined}
-      left={undefined}
-      box={undefined}
+    <OnBack
+      asset={props.asset || ({} as Asset)}
+      isFavoritesEnabled={props.isFavoritesEnabled || false}
       onBack={jest.fn()}
-      isOnSale
-      {...props}
     />,
     {
       preloadedState: {
@@ -39,7 +34,7 @@ function renderBaseDetail(props: Partial<BaseDetailProps> = {}) {
   )
 }
 
-describe('BaseDetail', () => {
+describe('OnBack', () => {
   let asset: Asset
   let useMobileMediaQueryMock: jest.MockedFunction<typeof useMobileMediaQuery>
 
@@ -55,9 +50,10 @@ describe('BaseDetail', () => {
       useMobileMediaQueryMock.mockReturnValue(false)
     })
 
-    it('should not render the favorites counter in the top-header', async () => {
-      const { getByTestId } = renderBaseDetail({
-        asset
+    it('should not render the favorites counter', () => {
+      const { queryByTestId } = renderOnBack({
+        asset,
+        isFavoritesEnabled: false
       })
       expect(
         within(
@@ -72,14 +68,11 @@ describe('BaseDetail', () => {
       useMobileMediaQueryMock.mockReturnValue(true)
     })
 
-    describe('and the asset is an nft', () => {
-      beforeEach(() => {
-        asset = { ...asset, tokenId: 'tokenId' } as Asset
-      })
-
-      it('should not render the favorites counter in the top-header', () => {
-        const { getByTestId } = renderBaseDetail({
-          asset
+    describe('and the favorites feature flag is not enabled', () => {
+      it('should not render the favorites counter', () => {
+        const { queryByTestId } = renderOnBack({
+          asset,
+          isFavoritesEnabled: false
         })
         expect(
           within(
@@ -89,14 +82,33 @@ describe('BaseDetail', () => {
       })
     })
 
-    describe('and the asset is an item', () => {
-      beforeEach(() => {
-        asset = { ...asset, itemId: 'itemId' } as Asset
+    describe('and the favorites feature flag is enabled', () => {
+      describe('and the asset is an nft', () => {
+        beforeEach(() => {
+          asset = { ...asset, tokenId: 'tokenId' } as Asset
+        })
+
+        it('should not render the favorites counter', () => {
+          const { queryByTestId } = renderOnBack({
+            asset,
+            isFavoritesEnabled: true
+          })
+          expect(queryByTestId(FAVORITES_COUNTER_TEST_ID)).toBeNull()
+        })
       })
 
-      it('should render the favorites counter', () => {
-        const { getByTestId } = renderBaseDetail({
-          asset
+      describe('and the asset is an item', () => {
+        beforeEach(() => {
+          asset = { ...asset, itemId: 'itemId' } as Asset
+          useMobileMediaQueryMock.mockReturnValue(true)
+        })
+
+        it('should render the favorites counter', () => {
+          const { getByTestId } = renderOnBack({
+            asset,
+            isFavoritesEnabled: true
+          })
+          expect(getByTestId(FAVORITES_COUNTER_TEST_ID)).toBeInTheDocument()
         })
         expect(
           within(
