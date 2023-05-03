af4e7f36c76081f4cc71eab8e7f792b4651af7ab
Florencia Barreto <florbarrlad@gmail.com>
2023-05-03 20:17:37 -0300

    fix: some style issues

--- webapp/src/components/ManaToFiat/ManaToFiat.tsx
+++ webapp/src/components/ManaToFiat/ManaToFiat.tsx
@@ -3,9 +3,13 @@ import { utils } from 'ethers'
 import { TokenConverter } from '../../modules/vendor/TokenConverter'
 import { Props } from './ManaToFiat.types'
 
+const ONE_MILLION = 1000000
+const ONE_BILLION = 1000000000
+const ONE_TRILLION = 1000000000000
+
 const ManaToFiat = (props: Props) => {
   const { mana, digits = 2 } = props
-  const [fiatValue, setFiatValue] = React.useState<number>()
+  const [fiatValue, setFiatValue] = React.useState<string>()
 
   useEffect(() => {
     try {
@@ -13,22 +17,48 @@ const ManaToFiat = (props: Props) => {
       new TokenConverter()
         .marketMANAToUSD(value)
         .then(usd => {
-          setFiatValue(usd)
+          if (usd > ONE_TRILLION) {
+            setFiatValue(
+              `$ ${(+(+usd / ONE_TRILLION).toFixed(digits)).toLocaleString(
+                undefined,
+                {
+                  maximumFractionDigits: 2
+                }
+              )}T`
+            )
+          } else if (usd > ONE_BILLION) {
+            setFiatValue(
+              `$ ${(+(+usd / ONE_BILLION).toFixed(digits)).toLocaleString(
+                undefined,
+                {
+                  maximumFractionDigits: 2
+                }
+              )}B`
+            )
+          } else if (usd > ONE_MILLION) {
+            setFiatValue(
+              `$ ${(+(+usd / ONE_MILLION).toFixed(digits)).toLocaleString(
+                undefined,
+                {
+                  maximumFractionDigits: 2
+                }
+              )}M`
+            )
+          } else {
+            setFiatValue(
+              `$ ${(+usd.toFixed(digits)).toLocaleString(undefined, {
+                maximumFractionDigits: 2
+              })}`
+            )
+          }
         })
         .catch()
     } catch (error) {
       // do nothing
     }
-  }, [mana])
+  }, [digits, mana])
 
-  return fiatValue ? (
-    <>
-      $
-      {Number(fiatValue.toFixed(digits)).toLocaleString(undefined, {
-        maximumFractionDigits: 2
-      })}
-    </>
-  ) : null
+  return fiatValue ? <>{fiatValue}</> : null
 }
 
 export default React.memo(ManaToFiat)
