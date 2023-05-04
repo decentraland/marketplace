5f189adad510d7b9881a2f3044cf6bbe72517ff2
Lautaro Petaccio <lausuper@gmail.com>
2023-05-04 09:50:44 -0300

    fix: Remove undefined check

--- webapp/src/components/AssetList/AssetList.tsx
+++ webapp/src/components/AssetList/AssetList.tsx
@@ -56,9 +56,7 @@ const AssetList = (props: Props) => {
   const maxQuerySize = getMaxQuerySize(vendor)
 
   const hasMorePages =
-    count !== undefined &&
-    (assets.length !== count || count === maxQuerySize) &&
-    page <= MAX_PAGE
+    (assets.length !== count || count === maxQuerySize) && page <= MAX_PAGE
 
   const emptyStateTranslationString = useMemo(() => {
     if (assets.length > 0) {
