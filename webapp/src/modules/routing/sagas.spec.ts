216bf4e5d1d03af440534156b6b6c3050076e980
Florencia Barreto <florbarrlad@gmail.com>
2023-05-09 08:55:49 -0300

    fix: comment

--- webapp/src/modules/routing/sagas.spec.ts
+++ webapp/src/modules/routing/sagas.spec.ts
@@ -80,7 +80,6 @@ describe('when handling the clear filters request action', () => {
     browseOptionsWithoutFilters.page = 1
     pathname = 'aPath'
   })
-
   describe('and the filters are set', () => {
     it('should build a browseURL without the filter values that got resetted', () => {
       return expectSaga(routingSaga)
