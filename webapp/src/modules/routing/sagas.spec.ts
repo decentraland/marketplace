eae6e6483bc4af5046db21dac3c9f961864888da
Florencia Barreto <32873485+flobarreto@users.noreply.github.com>
2023-05-09 09:51:01 -0300

    feat: added empty state (#1664)
    * feat: added empty state
    
    * feat: fixed filter search
    
    * fix: tests
    
    * fix: test
    
    * feat: merge with base branch
    
    * fix: some styles on itemDetailPage
    
    * fix: tests
    
    * fix: make mana icon wrap if does not fit in card
    
    * fix: removed test typo
    
    * fix: comment
    
    * fix: comment
    
    * fix: listings table total

--- webapp/src/modules/routing/sagas.spec.ts
+++ webapp/src/modules/routing/sagas.spec.ts
@@ -9,6 +9,7 @@ import {
 import { getLocation, push } from 'connected-react-router'
 import { expectSaga } from 'redux-saga-test-plan'
 import { call, select } from 'redux-saga/effects'
+import { AssetStatusFilter } from '../../utils/filters'
 import { AssetType } from '../asset/types'
 import { getData as getEventData } from '../event/selectors'
 import { fetchFavoritedItemsRequest } from '../favorites/actions'
@@ -139,7 +140,8 @@ describe('when handling the clear filters request action', () => {
               buildBrowseURL(pathname, {
                 ...browseOptionsWithoutFilters,
                 section: Section.COLLECTIONS,
-                onlyOnSale: true
+                onlyOnSale: true,
+                status: AssetStatusFilter.ON_SALE
               })
             )
           )
