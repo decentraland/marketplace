2b69d6e0fde5d129b908ef5a5fb5e604e63f7aac
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-05-19 11:51:02 +0200

    feat: Add Lists V1 FF

--- webapp/src/modules/features/types.ts
+++ webapp/src/modules/features/types.ts
@@ -13,5 +13,6 @@ export enum FeatureName {
   RENTAL_PERIOD_FILTER = 'rental-period-filter',
   MAP_VIEW_FILTERS = 'map-view-filters',
   RENTAL_PRICE_FILTER_CHART = 'rental-price-fitler-chart',
-  FAVORITES = 'favorites'
+  FAVORITES = 'favorites,',
+  LISTS_V1 = 'lists-v1'
 }
