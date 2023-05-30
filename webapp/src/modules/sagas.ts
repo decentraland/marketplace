5ac3f486b87e95b9bb04089e217e5aa19cf00479
Nando <24811313+fzavalia@users.noreply.github.com>
2023-05-30 12:32:19 -0300

    feat: Remove dapps

--- webapp/src/modules/sagas.ts
+++ webapp/src/modules/sagas.ts
@@ -89,11 +89,7 @@ export function* rootSaga(getIdentity: () => AuthIdentity | undefined) {
     marketplaceAnalyticsSagas(),
     featuresSaga({
       polling: {
-        apps: [
-          ApplicationName.MARKETPLACE,
-          ApplicationName.BUILDER,
-          ApplicationName.DAPPS
-        ],
+        apps: [ApplicationName.MARKETPLACE, ApplicationName.BUILDER],
         delay: 60000 /** 60 seconds */
       }
     }),
