87819ae7c03b702c15cda416ed52e74c996e6171
Florencia Barreto <florbarrlad@gmail.com>
2023-05-30 13:22:26 -0300

    fix: removed if on thumbnail item

--- webapp/src/components/AssetImage/utils.tsx
+++ webapp/src/components/AssetImage/utils.tsx
@@ -2,10 +2,6 @@ import { Item } from '@dcl/schemas'
 
 export const getEthereumItemUrn = (item: Item) => {
   let regex = /urn:decentraland:ethereum:collections-v1:[^/]+/
-
-  if ('thumbnail' in item) {
-    const match = item.thumbnail.match(regex)
-    return match ? match[0] : ''
-  }
-  return ''
+  const match = item.thumbnail.match(regex)
+  return match ? match[0] : ''
 }
