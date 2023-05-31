9608897c7c0d22bcec2a2646372e7bb889137740
Florencia Barreto <florbarrlad@gmail.com>
2023-05-31 12:10:41 -0300

    fix: description

--- webapp/src/components/AssetPage/Description/Description.tsx
+++ webapp/src/components/AssetPage/Description/Description.tsx
@@ -4,7 +4,7 @@ import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Props } from './Description.types'
 import styles from './Description.module.css'
 
-const MAX_LENGTH = 81
+const MAX_LENGTH = 148
 
 const Description = (props: Props) => {
   const { text } = props
@@ -14,22 +14,21 @@ const Description = (props: Props) => {
   return (
     <div className={styles.Description}>
       <Header sub>{t('asset_page.description')}</Header>
-      <div
-        className={
-          hasMoreLines && showMore
-            ? styles.descriptionText
-            : styles.descriptionContained
-        }
-        id="text-container"
-      >
-        {text ? text : t('asset_page.no_description')}
+      <div className={styles.descriptionText} id="text-container">
+        {text
+          ? hasMoreLines && !showMore
+            ? `${text.substring(0, MAX_LENGTH)}...`
+            : text
+          : t('asset_page.no_description')}
       </div>
       {hasMoreLines ? (
         <span
           onClick={() => setShowMore(prevState => !prevState)}
           className={styles.readMore}
         >
-          {t('asset_page.read_more').toLocaleUpperCase()}
+          {showMore
+            ? t('asset_page.read_less').toLocaleUpperCase()
+            : t('asset_page.read_more').toLocaleUpperCase()}
         </span>
       ) : null}
     </div>
