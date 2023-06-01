9ac0098d86ea46a9a5aff7dd48599a2fcfc75532
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-06-01 19:21:56 -0300

    Revert "feat: add authorization modal to sell action" (#1754)

--- webapp/src/components/SellPage/SellModal/SellModal.types.ts
+++ webapp/src/components/SellPage/SellModal/SellModal.types.ts
@@ -1,8 +1,8 @@
 import { Wallet } from 'decentraland-dapps/dist/modules/wallet/types'
 import { Order } from '@dcl/schemas'
-import { WithAuthorizedActionProps } from 'decentraland-dapps/dist/containers/withAuthorizedAction'
+import { Authorization } from 'decentraland-dapps/dist/modules/authorization/types'
 import { NFT } from '../../../modules/nft/types'
-import { clearOrderErrors, createOrderRequest } from '../../../modules/order/actions'
+import { createOrderRequest } from '../../../modules/order/actions'
 import { Contract } from '../../../modules/vendor/services'
 import { getContract } from '../../../modules/contract/selectors'
 
@@ -10,10 +10,10 @@ export type Props = {
   nft: NFT
   order: Order | null
   wallet: Wallet | null
+  authorizations: Authorization[]
   isLoading: boolean
   isCreatingOrder: boolean
   getContract: (query: Partial<Contract>) => ReturnType<typeof getContract>
   onGoBack: () => void
   onCreateOrder: typeof createOrderRequest
-  onClearOrderErrors: typeof clearOrderErrors
-} & WithAuthorizedActionProps
+}
