9fcd29b287e5d6ee35d3130f0e086fd33eecd2e9
Lautaro Petaccio <lausuper@gmail.com>
2023-06-12 11:52:13 -0300

    feat: Add private tag

---
+++ webapp/src/components/PrivateTag/PrivateTag.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import classnames from 'classnames'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
+import { Props } from './PrivateTag.types'
+import styles from './PrivateTag.module.css'
+
+const PrivateTag = (props: Props) => {
+  return (
+    <div
+      className={classnames(styles.private, props.className)}
+      data-testid={props['data-testid']}
+    >
+      <div className={styles.icon}></div>
+      {t('list_card.private')}
+    </div>
+  )
+}
+
+export default React.memo(PrivateTag)
