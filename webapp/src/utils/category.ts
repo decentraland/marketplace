880ffad1edc07f1745a35e0e5835938c888a74c5
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2023-07-12 11:28:53 -0300

    feat: add category to buy event

---
+++ webapp/src/utils/category.ts
@@ -0,0 +1,13 @@
+import { NFTCategory } from '@dcl/schemas'
+import { NFT } from '../modules/nft/types'
+
+export function getCategoryInfo(nft: NFT) {
+  switch (nft.category) {
+    case NFTCategory.EMOTE:
+      return { emoteCategory: nft.data.emote?.category }
+    case NFTCategory.WEARABLE:
+      return { wearableCategory: nft.data.wearable?.category }
+    default: 
+      return {}
+  }
+}
