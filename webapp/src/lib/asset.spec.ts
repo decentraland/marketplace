726d424dad25bca319efdc4289e20d7ae9fe34c9
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-07-27 14:10:45 +0200

    feat: Fetch sw public content (#1946)
    * feat: Fetch item content to take the video from builder api
    
    * fix: Use asset itemid only
    
    * fix: Visual fixes in the showcase modal
    
    * feat: Add EOL in css file

--- webapp/src/lib/asset.spec.ts
+++ webapp/src/lib/asset.spec.ts
@@ -1,11 +1,24 @@
 import * as contentClient from 'dcl-catalyst-client/dist/client/ContentClient'
+import { builderAPI } from '../modules/vendor/decentraland/builder/api'
+import { Asset } from '../modules/asset/types'
 import {
   getSmartWearableRequiredPermissions,
   getSmartWearableSceneContent,
   getSmartWearableVideoShowcase
 } from './asset'
 
+jest.mock('../modules/vendor/decentraland/builder/api', () => ({
+  builderAPI: {
+    fetchItemContent: jest.fn()
+  }
+}))
+
 const anSWUrn = 'aUrn'
+const smartWearable = {
+  contractAddress: '0xcontractAddress',
+  itemId: 'itemId',
+  urn: anSWUrn
+} as Asset
 const entity = [{ content: [{ file: 'scene.json', hash: 'aHash' }] }]
 let SWSceneContent = {
   main: 'bin/game.js',
@@ -15,18 +28,20 @@ let SWSceneContent = {
   },
   requiredPermissions: ['USE_WEB3_API', 'OPEN_EXTERNAL_LINK']
 }
-let mockClient: jest.SpyInstance
+let clientMock: jest.SpyInstance
+let fetchItemContentMock: jest.Mock
 let SWSceneContentBuffer: ArrayBuffer
 
 beforeEach(() => {
-  mockClient = jest.spyOn(contentClient, 'createContentClient')
+  clientMock = jest.spyOn(contentClient, 'createContentClient')
+  fetchItemContentMock = builderAPI.fetchItemContent as jest.Mock
   SWSceneContentBuffer = Buffer.from(JSON.stringify(SWSceneContent))
 })
 
 describe('when getting a smart wearable scene content', () => {
   describe('and the smart wearable does not have an entity', () => {
     beforeEach(() => {
-      mockClient = mockClient.mockReturnValueOnce({
+      clientMock.mockReturnValueOnce({
         fetchEntitiesByPointers: jest.fn().mockResolvedValueOnce([])
       })
     })
@@ -38,7 +53,7 @@ describe('when getting a smart wearable scene content', () => {
 
   describe('and the smart wearable does not have a valid entity', () => {
     beforeEach(() => {
-      mockClient.mockReturnValueOnce({
+      clientMock.mockReturnValueOnce({
         fetchEntitiesByPointers: jest
           .fn()
           .mockResolvedValueOnce([{ id: 'anId' }])
@@ -52,7 +67,7 @@ describe('when getting a smart wearable scene content', () => {
 
   describe('and the smart wearable have a valid entity', () => {
     beforeEach(() => {
-      mockClient.mockReturnValueOnce({
+      clientMock.mockReturnValueOnce({
         fetchEntitiesByPointers: jest.fn().mockResolvedValueOnce(entity),
         downloadContent: jest.fn().mockResolvedValueOnce(SWSceneContentBuffer)
       })
@@ -71,7 +86,7 @@ describe('when getting a smart wearable required permissions', () => {
     beforeEach(() => {
       SWSceneContent = { ...SWSceneContent, requiredPermissions: [] }
       SWSceneContentBuffer = Buffer.from(JSON.stringify(SWSceneContent))
-      mockClient.mockReturnValueOnce({
+      clientMock.mockReturnValueOnce({
         fetchEntitiesByPointers: jest.fn().mockResolvedValueOnce(entity),
         downloadContent: jest.fn().mockResolvedValueOnce(SWSceneContentBuffer)
       })
@@ -86,7 +101,7 @@ describe('when getting a smart wearable required permissions', () => {
 
   describe('and the smart wearable have required permissions', () => {
     beforeEach(() => {
-      mockClient.mockReturnValueOnce({
+      clientMock.mockReturnValueOnce({
         fetchEntitiesByPointers: jest.fn().mockResolvedValueOnce(entity),
         downloadContent: jest.fn().mockResolvedValueOnce(SWSceneContentBuffer)
       })
@@ -101,52 +116,51 @@ describe('when getting a smart wearable required permissions', () => {
 })
 
 describe('when getting a smart wearable video showcase', () => {
-  describe('and the smart wearable does not have an entity', () => {
-    beforeEach(() => {
-      mockClient = mockClient.mockReturnValueOnce({
-        fetchEntitiesByPointers: jest.fn().mockResolvedValueOnce([])
-      })
-    })
-
+  describe.each([null, undefined])('and the asset itemId is %s', itemId => {
     it('should return undefined', async () => {
-      expect(await getSmartWearableVideoShowcase(anSWUrn)).toBe(undefined)
+      expect(
+        await getSmartWearableVideoShowcase({
+          ...smartWearable,
+          itemId
+        } as Asset)
+      ).toBe(undefined)
     })
   })
 
-  describe('and the smart wearable has an entity', () => {
-    describe('and the smart wearable does not have the video in its content', () => {
-      beforeEach(() => {
-        mockClient.mockReturnValueOnce({
-          fetchEntitiesByPointers: jest.fn().mockResolvedValueOnce(entity)
-        })
-      })
-
-      it('should return undefined', async () => {
-        expect(await getSmartWearableVideoShowcase(anSWUrn)).toBeUndefined()
-      })
+  describe('and the builder api fails', () => {
+    beforeEach(() => {
+      fetchItemContentMock.mockRejectedValueOnce(new Error('aError'))
     })
 
-    describe('and the smart wearable have video showcase', () => {
-      const entityWithVideo = [
-        {
-          content: [
-            { file: 'scene.json', hash: 'aHash' },
-            { file: 'video.mp4', hash: 'aVideoHash' }
-          ]
-        }
-      ]
+    it('should return undefined', async () => {
+      expect(await getSmartWearableVideoShowcase(smartWearable)).toBe(undefined)
+    })
+  })
 
-      beforeEach(() => {
-        mockClient.mockReturnValueOnce({
-          fetchEntitiesByPointers: jest
-            .fn()
-            .mockResolvedValueOnce(entityWithVideo)
-        })
-      })
+  describe('and the smart wearable does not have a video in the content', () => {
+    beforeEach(() => {
+      fetchItemContentMock.mockResolvedValueOnce({})
+    })
 
-      it('should return the video hash', async () => {
-        expect(await getSmartWearableVideoShowcase(anSWUrn)).toBe('aVideoHash')
-      })
+    it('should return undefined', async () => {
+      expect(await getSmartWearableVideoShowcase(smartWearable)).toBe(undefined)
+    })
+  })
+
+  describe('and the smart wearable has a video', () => {
+    const content = {
+      'scene.json': 'aHash',
+      'video.mp4': 'aVideoHash'
+    }
+
+    beforeEach(() => {
+      fetchItemContentMock.mockResolvedValueOnce(content)
+    })
+
+    it('should return the video hash', async () => {
+      expect(await getSmartWearableVideoShowcase(smartWearable)).toBe(
+        'aVideoHash'
+      )
     })
   })
 })
