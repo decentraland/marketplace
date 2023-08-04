f0d454059dfdffba1cb10dd161ca3823c862ad76
Nando <24811313+fzavalia@users.noreply.github.com>
2023-08-04 14:37:17 -0300

    fix: Create client stub for tests

---
+++ webapp/src/lib/@dcl/single-sign-on-client.ts
@@ -0,0 +1,9 @@
+// This is only used for testing as the single-sign-on-client is an ESM and jest do not support them
+
+export const init = jest.fn()
+
+export const getIdentity = jest.fn()
+
+export const storeIdentity = jest.fn()
+
+export const clearIdentity = jest.fn()
