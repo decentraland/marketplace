3b3c9b3bf740430292fc890b37046642e356e392
Nando <24811313+fzavalia@users.noreply.github.com>
2023-08-04 17:10:57 -0300

    fix: Remove stub lib

--- webapp/src/lib/@dcl/single-sign-on-client.ts
+++
@@ -1,9 +0,0 @@
-// This is only used for testing as the single-sign-on-client is an ESM and jest do not support them
-
-export const init = jest.fn()
-
-export const getIdentity = jest.fn()
-
-export const storeIdentity = jest.fn()
-
-export const clearIdentity = jest.fn()
