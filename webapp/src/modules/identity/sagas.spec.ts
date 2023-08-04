9696327441af434d3047ad2b5228ded3b14d1163
Nando <24811313+fzavalia@users.noreply.github.com>
2023-08-04 14:40:01 -0300

    fix: Tests

--- webapp/src/modules/identity/sagas.spec.ts
+++ webapp/src/modules/identity/sagas.spec.ts
@@ -1,14 +1,18 @@
 import { expectSaga } from 'redux-saga-test-plan'
-import { put } from 'redux-saga-test-plan/matchers'
-import { select } from 'redux-saga/effects'
+import { call } from 'redux-saga/effects'
 import { Wallet } from 'decentraland-dapps/dist/modules/wallet/types'
 import { connectWalletSuccess } from 'decentraland-dapps/dist/modules/wallet/actions'
-import { getCurrentIdentity } from './selectors'
+import * as SingleSignOn from '@dcl/single-sign-on-client'
 import { identitySaga } from './sagas'
-import { generateIdentityRequest } from './actions'
+import { generateIdentityRequest, generateIdentitySuccess } from './actions'
+
+jest.mock('@dcl/single-sign-on-client')
+
+const SingleSignOnMock = SingleSignOn as jest.Mocked<typeof SingleSignOn>
 
 describe('when handling the wallet connection success', () => {
   let wallet: Wallet
+
   beforeEach(() => {
     wallet = {
       address: '0x0'
@@ -19,8 +23,7 @@ describe('when handling the wallet connection success', () => {
     it('should put an action to generate the identity', () => {
       return expectSaga(identitySaga)
         .provide([
-          [select(getCurrentIdentity), null],
-          [put(generateIdentityRequest(wallet.address)), undefined]
+          [call([SingleSignOnMock, 'getIdentity'], wallet.address), null]
         ])
         .put(generateIdentityRequest(wallet.address))
         .dispatch(connectWalletSuccess(wallet))
@@ -29,10 +32,14 @@ describe('when handling the wallet connection success', () => {
   })
 
   describe("and there's an identity", () => {
-    it('should not put an action to generate the identity', () => {
+    it('should put an action to store the identity', () => {
+      const identity = {} as any
+
       return expectSaga(identitySaga)
-        .provide([[select(getCurrentIdentity), {}]])
-        .not.put(generateIdentityRequest(wallet.address))
+        .provide([
+          [call([SingleSignOnMock, 'getIdentity'], wallet.address), identity]
+        ])
+        .put(generateIdentitySuccess(wallet.address, identity))
         .dispatch(connectWalletSuccess(wallet))
         .run({ silenceTimeout: true })
     })
