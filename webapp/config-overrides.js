e511cd454c3c2860b745a4b4e69611f6eb08cc99
Nando <24811313+fzavalia@users.noreply.github.com>
2023-08-04 14:36:58 -0300

    fix: Support client

--- webapp/config-overrides.js
+++ webapp/config-overrides.js
@@ -6,6 +6,7 @@ module.exports = override(
     path.resolve(__dirname, 'src'),
     path.resolve(__dirname, 'node_modules/react-virtualized-auto-sizer'),
     path.resolve(__dirname, 'node_modules/decentraland-connect/node_modules/@walletconnect'),
-    path.resolve(__dirname, 'node_modules/@walletconnect')
+    path.resolve(__dirname, 'node_modules/@walletconnect'),
+    path.resolve(__dirname, 'node_modules/@dcl/single-sign-on-client'),
   ])
 )
