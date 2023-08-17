49c036a553b45f954c4a2b0a610750e326fde914
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-08-17 18:21:14 -0300

    chore: override prettier rules to match marketplace prettier

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -1,6 +1,23 @@
+/* eslint-env node */
 module.exports = {
   extends: ['@dcl/eslint-config/dapps'],
   parserOptions: {
     project: ['tsconfig.json']
-  }
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.ts', '*.tsx'],
+      rules: {
+        ['prettier/prettier']: [
+          'error',
+          {
+            printWidth: 80,
+            singleQuote: true,
+            semi: false,
+            trailingComma: 'none'
+          }
+        ]
+      }
+    }
+  ]
 }
