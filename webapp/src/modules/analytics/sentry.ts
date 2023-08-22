0cd5d2184e15146ab35ef31dc38fa08612f6e9eb
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-08-22 10:54:52 +0200

    feat: Configure Sentry

--- webapp/src/modules/analytics/sentry.ts
+++ webapp/src/modules/analytics/sentry.ts
@@ -1,15 +1,20 @@
 import { init, BrowserTracing, Replay } from '@sentry/react'
+import { config } from '../../config'
 
 init({
-  dsn: '<your_DSN_key>',
-  integrations: [new BrowserTracing(), new Replay()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  tracesSampleRate: 1.0,
-
-  // Capture Replay for 10% of all sessions,
-  // plus for 100% of sessions with an error
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0
+  environment: config.get('ENVIRONMENT'),
+  release: '6.6.7',
+  dsn: config.get('SENTRY_DSN'),
+  integrations: [
+    new BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [config.get('MARKETPLACE_URL')]
+    }),
+    new Replay()
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 })
