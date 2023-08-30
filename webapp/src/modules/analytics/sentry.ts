648ea04432ddd508b77499fce25d4bc446831225
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2023-08-30 17:27:28 +0200

    chore: Lower Sentry Replay sample rates (#1995)
    * Chore: Lower Sentry Replay sample rates
    
    Signed-off-by: Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
    
    * chore: Remove comments
    
    Signed-off-by: Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>

--- webapp/src/modules/analytics/sentry.ts
+++ webapp/src/modules/analytics/sentry.ts
@@ -10,8 +10,8 @@ init({
     new Replay()
   ],
   // Performance Monitoring
-  tracesSampleRate: 0.001, // Capture 1% of the transactions
+  tracesSampleRate: 0.001,
   // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysSessionSampleRate: 0.01,
+  replaysOnErrorSampleRate: 0.01
 })
