7fe3c117beeed871451b362f75f441c3a9ba716e
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-09-12 09:09:21 -0300

    fix: YourOffer breaking mobile styles (#2010)

--- webapp/src/components/AssetPage/YourOffer/YourOffer.tsx
+++ webapp/src/components/AssetPage/YourOffer/YourOffer.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import classNames from 'classnames'
 import { Bid, Network } from '@dcl/schemas'
-import { Button, Divider, Popup } from 'decentraland-ui'
+import { Button, Divider, Popup, useMobileMediaQuery } from 'decentraland-ui'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import Mana from '../../Mana/Mana'
 import { formatDistanceToNow } from '../../../lib/date'
@@ -16,13 +17,93 @@ import styles from './YourOffer.module.css'
 
 const FIRST = '1'
 
+const Price = (props: { bid: Bid }) => {
+  const { bid } = props
+
+  return (
+    <div className={styles.column}>
+      <span className={styles.texts}>
+        {t('offers_table.offer').toUpperCase()}&nbsp;
+        <Popup
+          content={
+            bid.network === Network.MATIC
+              ? t('best_buying_option.minting.polygon_mana')
+              : t('best_buying_option.minting.ethereum_mana')
+          }
+          position="top center"
+          trigger={
+            <img
+              src={infoIcon}
+              alt="info"
+              className={styles.informationTooltip}
+            />
+          }
+          on="hover"
+        />
+      </span>
+      <div className={styles.row}>
+        <div className={styles.informationBold}>
+          <Mana
+            withTooltip
+            size="large"
+            network={bid.network}
+            className={styles.informationBold}
+          >
+            {formatWeiMANA(bid.price)}
+          </Mana>
+        </div>
+        {+bid.price > 0 && (
+          <div className={styles.informationText}>
+            &nbsp;{'('}
+            <ManaToFiat mana={bid.price} />
+            {')'}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+const PublishDate = (props: { bid: Bid }) => {
+  const { bid } = props
+  return (
+    <div className={`${styles.column} ${styles.center}`}>
+      <img src={calendar} alt="calendar" className={styles.calendar} />
+      <span className={styles.texts}>
+        {t('offers_table.date_published').toUpperCase()}
+      </span>
+
+      {formatDistanceToNow(+bid.createdAt, {
+        addSuffix: true
+      })}
+    </div>
+  )
+}
+
+const ExpirationDate = (props: { bid: Bid }) => {
+  const { bid } = props
+  return (
+    <div className={`${styles.column} ${styles.center}`}>
+      <img src={expiration} alt="expiration" className={styles.calendar} />
+      <span className={styles.texts}>
+        {t('offers_table.expiration_date').toUpperCase()}
+      </span>
+
+      {formatDistanceToNow(+bid.expiresAt, {
+        addSuffix: true
+      })}
+    </div>
+  )
+}
+
 const YourOffer = (props: Props) => {
   const { nft, address, onUpdate, onCancel } = props
 
   const [bid, setBid] = useState<Bid>()
+  const isMobile = useMobileMediaQuery()
 
   useEffect(() => {
-    if (nft) {
+    if (nft && address) {
       bidAPI
         .fetchByNFT(
           nft.contractAddress,
@@ -54,72 +135,24 @@ const YourOffer = (props: Props) => {
       </span>
 
       <Divider />
-      <div className={`${styles.row} ${styles.bottomInformationPadding}`}>
-        <div className={styles.column}>
-          <span className={styles.texts}>
-            {t('offers_table.offer').toUpperCase()}&nbsp;
-            <Popup
-              content={
-                bid.network === Network.MATIC
-                  ? t('best_buying_option.minting.polygon_mana')
-                  : t('best_buying_option.minting.ethereum_mana')
-              }
-              position="top center"
-              trigger={
-                <img
-                  src={infoIcon}
-                  alt="info"
-                  className={styles.informationTooltip}
-                />
-              }
-              on="hover"
-            />
-          </span>
-          <div className={styles.row}>
-            <div className={styles.informationBold}>
-              <Mana
-                withTooltip
-                size="large"
-                network={bid.network}
-                className={styles.informationBold}
-              >
-                {formatWeiMANA(bid.price)}
-              </Mana>
-            </div>
-            {+bid.price > 0 && (
-              <div className={styles.informationText}>
-                &nbsp;{'('}
-                <ManaToFiat mana={bid.price} />
-                {')'}
-              </div>
-            )}
+      <div className={styles.information}>
+        <Price bid={bid} />
+        {isMobile ? (
+          <div className={classNames(styles.row, styles.dates)}>
+            <PublishDate bid={bid} />
+            <ExpirationDate bid={bid} />
           </div>
-        </div>
-        <div className={`${styles.column} ${styles.center}`}>
-          <img src={calendar} alt="calendar" className={styles.calendar} />
-          <span className={styles.texts}>
-            {t('offers_table.date_published').toUpperCase()}
-          </span>
-
-          {formatDistanceToNow(+bid.createdAt, {
-            addSuffix: true
-          })}
-        </div>
-
-        <div className={`${styles.column} ${styles.center}`}>
-          <img src={expiration} alt="expiration" className={styles.calendar} />
-          <span className={styles.texts}>
-            {t('offers_table.expiration_date').toUpperCase()}
-          </span>
-
-          {formatDistanceToNow(+bid.expiresAt, {
-            addSuffix: true
-          })}
-        </div>
+        ) : (
+          <>
+            <PublishDate bid={bid} />
+            <ExpirationDate bid={bid} />
+          </>
+        )}
 
         <div className={styles.actionsContainer}>
           <Button
             inverted
+            fluid
             className={styles.actions}
             onClick={() => onCancel(bid)}
           >
@@ -127,6 +160,7 @@ const YourOffer = (props: Props) => {
           </Button>
           <Button
             primary
+            fluid
             className={styles.actions}
             onClick={() => onUpdate(bid)}
           >
