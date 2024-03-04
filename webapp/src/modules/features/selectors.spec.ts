e9d79a4d555463640ef4e962b9269947d117f46c
Lautaro Petaccio <lausuper@gmail.com>
2024-03-04 14:27:46 -0300

    fix: Remove conflict

--- webapp/src/modules/features/selectors.spec.ts
+++ webapp/src/modules/features/selectors.spec.ts
@@ -152,18 +152,6 @@ const waitForInitialLoadingSelectors = [
     selector: getIsSmartWearablesFTUEnabled
   },
   {
-<<<<<<< HEAD
-    name: 'isEmotesV2FTU',
-    feature: FeatureName.EMOTES_V2_FTU,
-    selector: getIsEmotesV2FTUEnabled
-=======
-    name: 'ens-address',
-    feature: FeatureName.ENS_ADDRESS,
-    selector: getIsEnsAddressEnabled,
-    applicationName: ApplicationName.DAPPS
->>>>>>> 9a45633513c047b7ed6bd054cc7edc8fd65d0d58
-  },
-  {
     name: 'chain-selector',
     feature: FeatureName.CHAIN_SELECTOR,
     selector: getIsChainSelectorEnabled,
