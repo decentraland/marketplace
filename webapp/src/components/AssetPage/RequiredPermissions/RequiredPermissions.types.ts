cdcc2951184a045fe905c7373e96c579176f4f1c
Lautaro Petaccio <lausuper@gmail.com>
2024-03-21 16:29:23 -0300

    fix: Don't refetch required permissions

--- webapp/src/components/AssetPage/RequiredPermissions/RequiredPermissions.types.ts
+++ webapp/src/components/AssetPage/RequiredPermissions/RequiredPermissions.types.ts
@@ -1,16 +1,18 @@
 import { Dispatch } from 'redux'
 import { Asset } from '../../../modules/asset/types'
-import { FetchSmartWearableRequiredPermissionsRequestAction } from '../../../modules/asset/actions'
+import { ClearAssetError, FetchSmartWearableRequiredPermissionsRequestAction } from '../../../modules/asset/actions'
 
 export type Props = {
   asset: Asset
+  error?: string
   requiredPermissions: string[]
   isLoading: boolean
   hasFetched: boolean
+  onClearError: () => void
   onFetchRequiredPermissions: (asset: Asset) => void
 }
 
 export type OwnProps = Pick<Props, 'asset'>
-export type MapStateProps = Pick<Props, 'requiredPermissions' | 'isLoading' | 'hasFetched'>
-export type MapDispatchProps = Pick<Props, 'onFetchRequiredPermissions'>
-export type MapDispatch = Dispatch<FetchSmartWearableRequiredPermissionsRequestAction>
+export type MapStateProps = Pick<Props, 'requiredPermissions' | 'isLoading' | 'hasFetched' | 'error'>
+export type MapDispatchProps = Pick<Props, 'onFetchRequiredPermissions' | 'onClearError'>
+export type MapDispatch = Dispatch<FetchSmartWearableRequiredPermissionsRequestAction | ClearAssetError>
