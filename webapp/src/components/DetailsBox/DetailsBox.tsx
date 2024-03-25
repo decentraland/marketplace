c7f8bdb61e6dfd63cf365f91107551c3a8a5e7bb
Juanma Hidalgo <juanma06@gmail.com>
2024-03-25 15:35:10 +0100

    feat: remove estate logic from details

--- webapp/src/components/DetailsBox/DetailsBox.tsx
+++ webapp/src/components/DetailsBox/DetailsBox.tsx
@@ -1,10 +1,7 @@
 import { memo } from 'react'
-import { Link } from 'react-router-dom'
-import { NFTCategory } from '@dcl/schemas'
 import classNames from 'classnames'
-import { T, t } from 'decentraland-dapps/dist/modules/translation/utils'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { isNFT } from '../../modules/asset/utils'
-import { locations } from '../../modules/routing/locations'
 import { Box } from '../AssetBrowse/Box'
 import { LinkedProfile } from '../LinkedProfile'
 import { Availability, Expiration, Type } from './DetailsRow'
@@ -16,7 +13,6 @@ export const DetailsBox = (props: Props) => {
   const { asset, order, rental, className } = props
 
   const owner = rental && rental.lessor ? rental.lessor : isNFT(asset) ? asset.owner : asset.creator
-  const isPartOfEstate = asset.category === NFTCategory.PARCEL && asset.data.parcel?.estate
 
   return (
     <Box header={t('details_box.title')} className={classNames(className)}>
@@ -25,20 +21,7 @@ export const DetailsBox = (props: Props) => {
         <Info title={t('details_box.network')}>
           <span>{asset.network}</span>
         </Info>
-        {isPartOfEstate && !!asset.data.parcel && isNFT(asset) && (
-          <div className={styles.estateInfo}>
-            <T
-              id="asset_page.part_of_estate"
-              values={{
-                estate_name: (
-                  <Link title={asset.data.parcel.estate!.name} to={locations.nft(asset.owner, asset.data.parcel.estate!.tokenId)}>
-                    {asset.data.parcel.estate!.name}
-                  </Link>
-                )
-              }}
-            />
-          </div>
-        )}
+
         <Availability asset={asset} />
         <Expiration asset={asset} order={order} rental={rental} />
         {owner ? (
