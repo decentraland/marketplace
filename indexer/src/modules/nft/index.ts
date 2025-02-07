a76f8c188e09771f5c4d121043fc6f77ee9001be
Juanma Hidalgo <juanma06@gmail.com>
2025-02-07 13:06:58 +0100

    fix: add missing cancelled logic for transferred orders

--- indexer/src/modules/nft/index.ts
+++ indexer/src/modules/nft/index.ts
@@ -63,8 +63,14 @@ export function clearNFTOrderProperties(nft: NFT): NFT {
 }
 
 export function cancelActiveOrder(nft: NFT, now: BigInt): boolean {
+  if (nft.activeOrder == null) {
+    return false
+  }
   let oldOrder = Order.load(nft.activeOrder)
-  if (oldOrder != null && oldOrder.status == status.OPEN) {
+  if (
+    oldOrder != null &&
+    (oldOrder.status == status.OPEN || oldOrder.status == status.TRANSFERRED)
+  ) {
     // Here we are setting old orders as cancelled, because the smart contract allows new orders to be created
     // and they just overwrite them in place. But the subgraph stores all orders ever
     // you can also overwrite ones that are expired
