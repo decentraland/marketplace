cc165d70064f4bb04dc6d6a81a488cae43441863
LautaroPetaccio <lausuper@gmail.com>
2025-02-18 15:30:21 -0300

    fix: Remove split chunks plugin

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -2,13 +2,13 @@ import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfil
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 import react from '@vitejs/plugin-react-swc'
 import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
-import { defineConfig, UserConfig, loadEnv, splitVendorChunkPlugin } from 'vite'
+import { defineConfig, UserConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const envVariables = loadEnv(mode, process.cwd())
   return {
-    plugins: [react(), splitVendorChunkPlugin()],
+    plugins: [react()],
     // Required because the CatalystClient tries to access it
     define: {
       // eslint-disable-next-line @typescript-eslint/naming-convention
