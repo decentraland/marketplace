5874fefba2cea116acb60e07ecadd5672f3db85a
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-06-18 17:53:09 -0300

    fix: Remove unused file

--- webapp/src/components/AssetImage/utils.tsx
+++
@@ -1,7 +0,0 @@
-import { Item } from '@dcl/schemas'
-
-export const getEthereumItemUrn = (item: Item) => {
-  const regex = /urn:decentraland:ethereum:collections-v1:[^/]+/
-  const match = item.thumbnail.match(regex)
-  return match ? match[0] : ''
-}
