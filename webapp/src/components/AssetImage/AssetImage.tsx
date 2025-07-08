e56c515bd720602a9e0b0c793300d9abfa52dc26
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-07-08 16:20:57 -0300

    fix: remove unrequired property

--- webapp/src/components/AssetImage/AssetImage.tsx
+++ webapp/src/components/AssetImage/AssetImage.tsx
@@ -456,7 +456,6 @@ const AssetImage = (props: Props) => {
               itemId={itemId}
               tokenId={tokenId}
               profile={avatar ? avatar.ethAddress : 'default'}
-              background={Rarity.getColor(emote!.rarity)}
               wheelZoom={1.5}
               wheelStart={100}
               onLoad={handleLoad}
