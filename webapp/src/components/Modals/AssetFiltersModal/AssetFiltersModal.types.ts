630452df06c5ef71f87e0b9817125ebee8b2ea85
LautaroPetaccio <lausuper@gmail.com>
2025-08-08 17:59:07 -0300

    fix: Refactor AssetFiltersModal container

--- webapp/src/components/Modals/AssetFiltersModal/AssetFiltersModal.types.ts
+++ webapp/src/components/Modals/AssetFiltersModal/AssetFiltersModal.types.ts
@@ -1,7 +1,6 @@
-import { Dispatch } from 'redux'
 import { ModalProps } from 'decentraland-ui/dist/components/Modal/Modal'
 import { AssetType } from '../../../modules/asset/types'
-import { BrowseAction, ClearFiltersAction } from '../../../modules/routing/actions'
+import { browse } from '../../../modules/routing/actions'
 import { BrowseOptions } from '../../../modules/routing/types'
 import { View } from '../../../modules/ui/types'
 
@@ -10,10 +9,5 @@ export type Props = ModalProps & {
   view?: View
   assetType: AssetType
   browseOptions: BrowseOptions
-  onBrowse: (options: BrowseOptions) => void
+  onBrowse: ActionFunction<typeof browse>
 }
-
-export type MapStateProps = Pick<Props, 'hasFiltersEnabled' | 'view' | 'assetType' | 'browseOptions'>
-
-export type MapDispatchProps = Pick<Props, 'onBrowse'>
-export type MapDispatch = Dispatch<ClearFiltersAction | BrowseAction>
