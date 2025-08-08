f60de736f35fd73dc6a075e32a49366daae94313
LautaroPetaccio <lausuper@gmail.com>
2025-08-08 17:54:36 -0300

    fix: Refactor Navigation container

--- webapp/src/components/Navigation/Navigation.types.ts
+++ webapp/src/components/Navigation/Navigation.types.ts
@@ -1,6 +1,5 @@
-import { Dispatch } from 'redux'
-import { openBuyManaWithFiatModalRequest, OpenBuyManaWithFiatModalRequestAction } from 'decentraland-dapps/dist/modules/gateway/actions'
-import { clearFilters, ClearFiltersAction } from '../../modules/routing/actions'
+import { openBuyManaWithFiatModalRequest } from 'decentraland-dapps/dist/modules/gateway/actions'
+import { clearFilters } from '../../modules/routing/actions'
 
 export enum NavigationTab {
   OVERVIEW = 'overview',
@@ -16,15 +15,11 @@ export enum NavigationTab {
 
 export type Props = {
   isCampaignBrowserEnabled: boolean
-  isFullScreen?: boolean
   activeTab?: NavigationTab
   isFullscreen?: boolean
   campaignTab?: string
-  onOpenBuyManaWithFiatModal: () => ReturnType<typeof openBuyManaWithFiatModalRequest>
-  onClearFilters: typeof clearFilters
+  onOpenBuyManaWithFiatModal: ActionFunction<typeof openBuyManaWithFiatModalRequest>
+  onClearFilters: ActionFunction<typeof clearFilters>
 }
 
-export type MapDispatch = Dispatch<OpenBuyManaWithFiatModalRequestAction | ClearFiltersAction>
-
-export type MapStateProps = Pick<Props, 'isCampaignBrowserEnabled' | 'isFullScreen' | 'campaignTab'>
-export type MapDispatchProps = Pick<Props, 'onOpenBuyManaWithFiatModal' | 'onClearFilters'>
+export type ContainerProps = Pick<Props, 'activeTab'>
