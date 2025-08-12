c90bc5bd2532ca0df7be9fbbe87277a501a49272
Braian Mellor <braianj@gmail.com>
2025-08-12 13:15:49 -0300

    test: new wearable preview

--- webapp/src/components/AssetImage/Preview/Preview.tsx
+++ webapp/src/components/AssetImage/Preview/Preview.tsx
@@ -297,6 +297,7 @@ export const Preview: React.FC<Props> = ({
       {showWearablePreview ? (
         <>
           <WearablePreview
+            baseUrl="https://wearable-preview-git-feat-configutaror-decentraland1.vercel.app"
             id="wearable-preview"
             background={Rarity.getColor(rarity)}
             emote={isTryingOnEnabled || isUnityRenderer ? previewEmote : undefined}
