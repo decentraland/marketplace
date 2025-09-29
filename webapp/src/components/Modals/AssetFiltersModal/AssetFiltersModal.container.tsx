cb12052202acb2f064830d57e2c68987abfc1399
Braian Mellor <braianj@gmail.com>
2025-09-29 12:25:07 -0300

    fix: close modal

--- webapp/src/components/Modals/AssetFiltersModal/AssetFiltersModal.container.tsx
+++ webapp/src/components/Modals/AssetFiltersModal/AssetFiltersModal.container.tsx
@@ -1,12 +1,13 @@
 import React, { useCallback } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { ModalProps } from 'decentraland-dapps/dist/providers/ModalProvider/ModalProvider.types'
 import { browse } from '../../../modules/routing/actions'
 import { useGetBrowseOptions } from '../../../modules/routing/hooks'
 import { hasFiltersEnabled } from '../../../modules/routing/url-parser'
 import { getView } from '../../../modules/ui/browse/selectors'
 import AssetFiltersModal from './AssetFiltersModal'
 
-export const AssetFiltersModalContainer: React.FC = () => {
+export const AssetFiltersModalContainer: React.FC<ModalProps> = props => {
   const dispatch = useDispatch()
   const browseOptions = useGetBrowseOptions()
   const view = useSelector(getView)
@@ -15,6 +16,7 @@ export const AssetFiltersModalContainer: React.FC = () => {
 
   return (
     <AssetFiltersModal
+      {...props}
       view={view}
       assetType={browseOptions.assetType}
       hasFiltersEnabled={hasFiltersEnabled(browseOptions)}
