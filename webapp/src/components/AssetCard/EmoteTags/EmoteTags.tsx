6144fb470f596a0f4858a504226df0018ec7560d
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-11-08 14:18:31 -0300

    feat: Add Social emote tag

--- webapp/src/components/AssetCard/EmoteTags/EmoteTags.tsx
+++ webapp/src/components/AssetCard/EmoteTags/EmoteTags.tsx
@@ -1,14 +1,16 @@
 import classNames from 'classnames'
 import { RarityBadge } from 'decentraland-dapps/dist/containers/RarityBadge'
-import { T } from 'decentraland-dapps/dist/modules/translation/utils'
+import { t, T } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Popup } from 'decentraland-ui'
+import socialSrc from '../../../images/emotes/social.svg'
 import soundSrc from '../../../images/emotes/sound.svg'
 import { Props } from './EmoteTags.types'
 import styles from './EmoteTags.module.css'
 
 const EmoteTags = (props: Props) => {
   const { asset } = props
-  const { rarity, loop, hasSound } = asset.data.emote || {}
+  const { rarity, loop, hasSound, outcomeType } = asset.data.emote || {}
+  const isSocial = !!outcomeType
 
   return (
     <div className={classNames([styles.EmoteTags, 'tags'])}>
@@ -29,6 +31,12 @@ const EmoteTags = (props: Props) => {
       {hasSound && (
         <div className={styles.sound}>
           <img src={soundSrc} alt="with sound" />
+        </div>
+      )}
+      {isSocial && (
+        <div className={styles.social}>
+          <img src={socialSrc} alt="social" />
+          {t('emote.social')}
         </div>
       )}
     </div>
