d2d49fd7a0102f0e2d66cd8b924581e3c9a0de15
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-12-12 11:03:44 -0300

    fix: Upgrade decentraland-ui2 package

--- webapp/src/components/AssetImage/Preview/Preview.tsx
+++ webapp/src/components/AssetImage/Preview/Preview.tsx
@@ -332,6 +332,7 @@ export const Preview: React.FC<Props> = ({
       {showWearablePreview ? (
         <>
           <WearablePreview
+            baseUrl="https://wearable-preview-2zm8cfg9g-decentraland1.vercel.app"
             id="wearable-preview"
             background={Rarity.getColor(rarity)}
             emote={isTryingOnEnabled || isUnityRenderer ? previewEmote : undefined}
