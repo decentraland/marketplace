2930d84d0fc174049cb3a94a3172111a93b5555f
Braian Mellor <braianj@gmail.com>
2025-12-17 17:42:32 -0300

    test: add mocks and fix test setup for React 18

--- webapp/src/tests/beforeSetupTests.ts
+++ webapp/src/tests/beforeSetupTests.ts
@@ -6,24 +6,68 @@ import path from 'path'
 import { TextEncoder, TextDecoder } from 'util'
 import { config } from 'dotenv'
 import flatten from 'flat'
+// eslint-disable-next-line import/order
 import fetch, { Request, Response } from 'node-fetch'
-import { en as dappsEn } from 'decentraland-dapps/dist/modules/translation/defaults'
-import * as translationUtils from 'decentraland-dapps/dist/modules/translation/utils'
-import * as locales from '../modules/translation/locales'
+/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires */
+jest.mock('file-saver', () => ({
+  saveAs: jest.fn()
+}))
+
+jest.mock('libsodium-wrappers-sumo', () => ({
+  ready: Promise.resolve(),
+  crypto_sign_keypair: jest.fn(),
+  crypto_sign_detached: jest.fn(),
+  crypto_sign_verify_detached: jest.fn(),
+  to_hex: jest.fn(),
+  from_hex: jest.fn()
+}))
 
 jest.mock('decentraland-dapps/dist/modules/translation/utils', () => {
-  const module = jest.requireActual<typeof translationUtils>('decentraland-dapps/dist/modules/translation/utils')
+  const module = jest.requireActual('decentraland-dapps/dist/modules/translation/utils')
   return {
     ...module,
     T: ({ id, values }: { id: string; values?: any }) => module.t(id, values)
   }
 })
 
-jest.mock('decentraland-ui2', () => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+jest.mock('decentraland-dapps/dist/containers/Navbar', () => {
+  const React = require('react')
+  const Navbar2Component = () => React.createElement('div', { 'data-testid': 'navbar2-mock' })
   return {
-    ...jest.requireActual('decentraland-ui2'),
-    Navbar2: () => jest.fn().mockReturnValue(null)
+    Navbar2: Navbar2Component
+  }
+})
+
+jest.mock('decentraland-ui2', () => {
+  const React = require('react')
+  const createStyledComponent = () => () => React.createElement('div')
+  const styledMock: any = () => createStyledComponent()
+  styledMock.div = () => createStyledComponent()
+  styledMock.span = () => createStyledComponent()
+  styledMock.button = () => createStyledComponent()
+  styledMock.a = () => createStyledComponent()
+  styledMock.nav = () => createStyledComponent()
+  styledMock.section = () => createStyledComponent()
+  styledMock.header = () => createStyledComponent()
+  const MockComponent = () => React.createElement('div')
+  return {
+    darkTheme: {},
+    lightTheme: {},
+    DclThemeProvider: ({ children }: { children: React.ReactNode }) => children,
+    Navbar2: MockComponent,
+    DownloadButton: MockComponent,
+    Switch: MockComponent,
+    AnimationControls: MockComponent,
+    EmoteControls: MockComponent,
+    WearablePreview: MockComponent,
+    ZoomControls: MockComponent,
+    ButtonGroup: MockComponent,
+    Button: MockComponent,
+    Menu: MockComponent,
+    MenuItem: MockComponent,
+    CreditsToggle: MockComponent,
+    JumpIn: MockComponent,
+    styled: styledMock
   }
 })
 
@@ -63,7 +107,7 @@ Object.defineProperty(window, 'matchMedia', {
 })
 
 config({ path: path.resolve(process.cwd(), '.env.example') })
-global.TextEncoder = TextEncoder
+global.TextEncoder = TextEncoder as any
 global.TextDecoder = TextDecoder as any
 if (!globalThis.fetch) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -84,4 +128,9 @@ global.FontFace = function () {
   add: () => Promise.resolve()
 } as any
 
+const { en: dappsEn } = require('decentraland-dapps/dist/modules/translation/defaults')
+const translationUtils = require('decentraland-dapps/dist/modules/translation/utils')
+const locales = require('../modules/translation/locales')
+
 translationUtils.setCurrentLocale('en', translationUtils.mergeTranslations(flatten(dappsEn), flatten(locales.en)))
+/* eslint-enable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires */
