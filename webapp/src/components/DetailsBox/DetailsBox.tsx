e8c9de7a929b3e1ee347921b15b369d4b98752bc
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2026-01-12 12:39:48 +0100

    fix: Show lessor as owner only if the rental is active (#2568)
    * fix: Show lessor as owner only if the rental is active
    
    * refactor: Send the whole rental to the fn

--- webapp/src/components/DetailsBox/DetailsBox.tsx
+++ webapp/src/components/DetailsBox/DetailsBox.tsx
@@ -1,5 +1,6 @@
 import { memo } from 'react'
 import classNames from 'classnames'
+import { type RentalListing, RentalStatus } from '@dcl/schemas'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { isNFT } from '../../modules/asset/utils'
 import { useGetCurrentOrder } from '../../modules/order/hooks'
@@ -10,11 +11,13 @@ import { Info } from './Info'
 import { Props } from './DetailsBox.types'
 import styles from './DetailsBox.module.css'
 
+const isActiveRental = ({ status }: RentalListing) => status === RentalStatus.OPEN || status === RentalStatus.EXECUTED
+
 export const DetailsBox = (props: Props) => {
   const { asset, rental, className } = props
   const order = useGetCurrentOrder()
 
-  const owner = rental && rental.lessor ? rental.lessor : isNFT(asset) ? asset.owner : asset.creator
+  const owner = rental?.lessor && isActiveRental(rental) ? rental.lessor : isNFT(asset) ? asset.owner : asset.creator
 
   return (
     <Box header={t('details_box.title')} className={classNames(className)}>
