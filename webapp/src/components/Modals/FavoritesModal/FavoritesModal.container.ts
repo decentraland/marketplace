f3eefafab2b12306920ad260e6022f049b9cddb3
Matias Pentreath <pentreathm@decentraland.org>
2026-02-04 12:29:10 -0300

    chore: update decentraland-ui2 to 1.3.4

--- webapp/src/components/Modals/FavoritesModal/FavoritesModal.container.ts
+++ webapp/src/components/Modals/FavoritesModal/FavoritesModal.container.ts
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux'
-import { AuthIdentity } from 'decentraland-crypto-fetch'
 import { getCurrentIdentity } from '../../../modules/identity/selectors'
 import { RootState } from '../../../modules/reducer'
 import FavoritesModal from './FavoritesModal'
@@ -7,7 +6,7 @@ import { MapStateProps } from './FavoritesModal.types'
 
 const mapState = (state: RootState): MapStateProps => {
   return {
-    identity: (getCurrentIdentity(state) as AuthIdentity | null) ?? undefined
+    identity: getCurrentIdentity(state) ?? undefined
   }
 }
 
