58416ec57e723b35188e78052cf2033942e85456
Juanma Hidalgo <juanma06@gmail.com>
2026-03-17 12:08:49 +0100

    feat: buildExplorerUrl uses config to get explorer url

--- webapp/src/modules/nft/parcel/utils.ts
+++ webapp/src/modules/nft/parcel/utils.ts
@@ -1,7 +1,9 @@
+import { config } from '../../../config'
+
 export function getId(x: number | string, y: number | string) {
   return x + ',' + y
 }
 
 export function buildExplorerUrl(x: string | number, y: string | number) {
-  return `https://decentraland.org/jump/?position=${x},${y}`
+  return `${config.get('EXPLORER_URL')}/?position=${x},${y}`
 }
