6e3a962988794fbf93cf2e7844a3364a75435378
Juanma Hidalgo <juanma06@gmail.com>
2026-03-17 11:58:15 +0100

    feat: update jump URL

--- webapp/src/modules/nft/parcel/utils.ts
+++ webapp/src/modules/nft/parcel/utils.ts
@@ -3,5 +3,5 @@ export function getId(x: number | string, y: number | string) {
 }
 
 export function buildExplorerUrl(x: string | number, y: string | number) {
-  return `https://play.decentraland.org/?position=${x},${y}`
+  return `https://decentraland.org/jump/?position=${x},${y}`
 }
